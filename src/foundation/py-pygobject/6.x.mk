469ee83c3a7a828e7f93c7fca7dae3becb9e463b
Greg Bruno <greg.bruno@stackiq.com>
2015-07-16 12:49:54 -0700

    more 6.x and 7.x goodness

--- src/foundation/py-pygobject/6.x.mk
+++ src/foundation/py-pygobject/6.x.mk
@@ -1,3 +1,4 @@
 SRCVERSION	= 2.14.1
 SRCFILE		= pygobject-$(SRCVERSION).tar.gz
 UNPACK		= gunzip -c
+PREP		= 
--- src/foundation/py-pygobject/7.x.mk
+++ src/foundation/py-pygobject/7.x.mk
@@ -1,3 +1,4 @@
 SRCVERSION	= 3.10.2
 SRCFILE		= pygobject-$(SRCVERSION).tar.xz
 UNPACK		= xzcat
+PREP		= yum install -y gobject-introspection-devel cairo-gobject-devel
