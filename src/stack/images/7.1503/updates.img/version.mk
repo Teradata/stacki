adb5345e79cd06acdf339351d79683c00d258fe6
Anoop Rajendra <anoop.rajendra@stackiq.com>
2015-10-23 13:05:24 -0700

    MariaDB, not MySQL

--- src/stack/images/7.1503/updates.img/version.mk
+++ src/stack/images/7.1503/updates.img/version.mk
@@ -1,5 +1,5 @@
 ORDER		= 98
 PKGROOT		= /opt/stack/images
 OVERLAY.PKGS	= foundation-py-wxPython foundation-python-xml \
-	foundation-mysql MegaCLI hpssacli storcli util-linux yum stack-wizard \
+	foundation-mariadb MegaCLI hpssacli storcli util-linux yum stack-wizard \
 	foundation-py-pexpect lighttpd
