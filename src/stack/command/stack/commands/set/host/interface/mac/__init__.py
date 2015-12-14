826ad753beb676bb1aaa56b597c0f241a0ec9a2b
Anoop Rajendra <anoop.rajendra@stackiq.com>
2015-12-14 12:22:29 -0800

    "set host interface mac" command was inadvertently removed as part of the last commit. This is not part of the Dell lightsout code.

---
+++ src/stack/command/stack/commands/set/host/interface/mac/__init__.py
@@ -0,0 +1,136 @@
+# @SI_Copyright@
+#                             www.stacki.com
+#                                  v2.0
+# 
+#      Copyright (c) 2006 - 2015 StackIQ Inc. All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#  
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#  
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice unmodified and in its entirety, this list of conditions and the
+# following disclaimer in the documentation and/or other materials provided 
+# with the distribution.
+#  
+# 3. All advertising and press materials, printed or electronic, mentioning
+# features or use of this software must display the following acknowledgement: 
+# 
+# 	 "This product includes software developed by StackIQ" 
+#  
+# 4. Except as permitted for the purposes of acknowledgment in paragraph 3,
+# neither the name or logo of this software nor the names of its
+# authors may be used to endorse or promote products derived from this
+# software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY STACKIQ AND CONTRIBUTORS ``AS IS''
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL STACKIQ OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# @SI_Copyright@
+#
+# @Copyright@
+#  				Rocks(r)
+#  		         www.rocksclusters.org
+#  		         version 5.4 (Maverick)
+#  
+# Copyright (c) 2000 - 2010 The Regents of the University of California.
+# All rights reserved.	
+#  
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#  
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#  
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice unmodified and in its entirety, this list of conditions and the
+# following disclaimer in the documentation and/or other materials provided 
+# with the distribution.
+#  
+# 3. All advertising and press materials, printed or electronic, mentioning
+# features or use of this software must display the following acknowledgement: 
+#  
+# 	"This product includes software developed by the Rocks(r)
+# 	Cluster Group at the San Diego Supercomputer Center at the
+# 	University of California, San Diego and its contributors."
+# 
+# 4. Except as permitted for the purposes of acknowledgment in paragraph 3,
+# neither the name or logo of this software nor the names of its
+# authors may be used to endorse or promote products derived from this
+# software without specific prior written permission.  The name of the
+# software includes the following terms, and any derivatives thereof:
+# "Rocks", "Rocks Clusters", and "Avalanche Installer".  For licensing of 
+# the associated name, interested parties should contact Technology 
+# Transfer & Intellectual Property Services, University of California, 
+# San Diego, 9500 Gilman Drive, Mail Code 0910, La Jolla, CA 92093-0910, 
+# Ph: (858) 534-5815, FAX: (858) 534-7345, E-MAIL:invent@ucsd.edu
+#  
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS''
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# @Copyright@
+
+import stack.commands
+from stack.exception import *
+
+class Command(stack.commands.set.host.command):
+	"""
+	Sets the mac address for named interface on host.
+
+	<arg type='string' name='host' repeat='1'>
+	Host name.
+	</arg>
+
+	<param type='string' name='interface' optional='0'>
+ 	Name of the interface.
+ 	</param>
+
+ 	<param type='string' name='mac' optional='0'>
+	The mac address of the interface. Usually of the form dd:dd:dd:dd:dd:dd
+	where d is a hex digit. This format is not enforced. Use mac=NULL to
+	clear the mac address.
+	</param>
+
+	<example cmd='set host interface mac backend-0-0 interface=eth1 mac=00:0e:0c:a7:5d:ff'>
+	Sets the MAC Address for the eth1 device on host backend-0-0.
+	</example>
+	"""
+	
+	def run(self, params, args):
+
+                (interface, mac) = self.fillParams([
+                        ('interface', None, True),
+                        ('mac',       None, True)
+                        ])
+
+		if not len(args):
+                        raise ArgRequired(self, 'host')
+
+                for host in self.getHostnames(args):
+			self.db.execute("""
+        	        	update networks, nodes set 
+				networks.mac=NULLIF('%s','NULL') where
+				nodes.name='%s' and networks.node=nodes.id and
+				networks.device like '%s'
+        	                """ % (mac, host, interface))
+
