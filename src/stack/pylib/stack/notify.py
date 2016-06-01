3775d42550cb8d221aef38b4a0a5a56c45886a01
Bill Sanders <billysanders@gmail.com>
2016-06-01 10:49:42 -0700

    Rename the notifications file

--- src/stack/pylib/stack/notify.py
+++ src/stack/pylib/stack/notify.py
@@ -6,7 +6,7 @@ def notify(msg):
 	''' generate some code to collect the contents of notify xml attrs as they are run
 	TODO: make this code do something useful intead of just append to a file :)
 	'''
-	filename = '/tmp/mynotif.txt'
+	filename = '/tmp/notifications.txt'
 	with open(filename, "a") as fi:
 		fi.write(msg + "\n")
 
