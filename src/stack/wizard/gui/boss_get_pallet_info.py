6074c86533882b8079ff3f3abdd131a60ccd57a7
Anoop Rajendra <anoop.rajendra@stackiq.com>
2016-10-18 14:40:11 -0700

    Remove unnecessary try/except block

--- src/stack/wizard/gui/boss_get_pallet_info.py
+++ src/stack/wizard/gui/boss_get_pallet_info.py
@@ -8,13 +8,9 @@ import pickle
 if __name__ == '__main__':
 	url = sys.argv[1]
 	url_cgi = url + '/pallets.cgi'
-	try:
-		response = urllib2.urlopen(url_cgi)
-		pallets = json.loads(response.read())
-		for pallet in pallets:
-			pallet['id'] = ''
-			pallet['url'] = str(url)
-		print pickle.dumps(pallets)
-	except:
-		raise
-		sys.exit(-1)
+	response = urllib2.urlopen(url_cgi)
+	pallets = json.loads(response.read())
+	for pallet in pallets:
+		pallet['id'] = ''
+		pallet['url'] = str(url)
+	print pickle.dumps(pallets)
