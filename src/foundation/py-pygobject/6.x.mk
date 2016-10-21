72616d4d3ace3fd00f406db727625b4952e88e4b
Anoop Rajendra <anoop.rajendra@stackiq.com>
2016-10-21 15:11:31 -0700

    Remove pygobject, and pygtk. No longer required for 6.x or 7.x

--- src/foundation/py-pygobject/6.x.mk
+++
@@ -1,4 +0,0 @@
-SRCVERSION	= 2.14.1
-SRCFILE		= pygobject-$(SRCVERSION).tar.gz
-UNPACK		= gunzip -c
-PREP		= 
--- src/foundation/py-pygobject/7.x.mk
+++
@@ -1,4 +0,0 @@
-SRCVERSION	= 3.10.2
-SRCFILE		= pygobject-$(SRCVERSION).tar.xz
-UNPACK		= xzcat
-PREP		= yum install -y gobject-introspection-devel cairo-gobject-devel
