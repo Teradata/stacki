8cff6a0737429139c111b59ea9f45013b46299a9
Anoop Rajendra <anoop.rajendra@stackiq.com>
2016-11-23 15:01:27 -0800

    Fixes for VLAN/Bridging/Bonding Don't use SQL queries to get interface information. Use "list host interface" instead

--- src/stack/command/stack/commands/report/host/interface/imp_redhat.py
+++ src/stack/command/stack/commands/report/host/interface/imp_redhat.py
@@ -13,38 +13,28 @@ class Implementation(stack.commands.Implementation):
 		
 		host = args[0]
 		
-		self.owner.db.execute("""select id, name, mask, mtu
-			from subnets""")
-
-		#
-		# need to prefetch the subnets data because we can't do a
-		# self.owner.db.execute() in the middle of a self.owner.db.fetchall() loop
-		# because it resets the MySQL cursor
-		#
-		subnets = {}
-		for row in self.owner.db.fetchall():
-			id = row[0]
-			subnets[id] = row
-
-		self.owner.db.execute("""select distinctrow 
-			net.mac, net.ip, net.device, net.vlanid,
-			net.subnet, net.module, net.options, net.channel,
-			s.gateway from networks net, nodes n, subnets s
-			where net.node = n.id and net.subnet = s.id 
-			and n.name = "%s" order by net.device""" % (host))
+		rows = self.owner.call('list.host.interface', [host, 'expanded=True'])
 
 		udev_output = ""
 
-		for row in self.owner.db.fetchall():
-			(mac, ip, device, vlanid, subnetid, module, options,
-				channel, gateway) = row
+		for row in rows:
 
-			netname = None
-			netmask = None
+			mac = row['mac']
+			ip  = row['ip']
+			device = row['interface']
+			netname = row['name']
+			netmask = row['mask']
+			subnet = row['network']
+			module = row['module']
+			options = row['options']
+			channel = row['channel']
+			gateway = row['gateway']
+			vlanid  = row['vlan']
+
 			mtu = None
-
-			if subnetid:
-				id, netname, netmask, mtu = subnets[subnetid]
+			if subnet:
+				subnetInfo = self.owner.call('list.network',[subnet])
+				mtu = subnetInfo[0]['mtu']
 
 			# Host attributes can override the subnets tables
 			# definition of the netmask.
@@ -75,48 +65,6 @@ class Implementation(stack.commands.Implementation):
 				#
 				self.owner.writeModprobe(host, device, module,
 					optionlist)
-
-			if vlanid:
-				#
-				# look up the name of the interface that
-				# maps to this VLAN spec
-				#
-				rows = self.owner.db.execute("""select net.device from
-					networks net, nodes n where
-					n.id = net.node and n.name = '%s'
-					and net.subnet = %d and
-					net.device not like 'vlan%%' """ %
-					(host, subnetid))
-
-				if rows:
-					dev, = self.owner.db.fetchone()
-					#
-					# check if already referencing 
-					# a physical device
-					#
-					if dev != device:
-						device = '%s.%d' % (dev, vlanid)
-
-			#
-			# for interfaces that have bridges attached, make sure
-			# we get the MTU of the network that is associated
-			# with the *bridge* and 
-			#
-			for opt in optionlist:
-				if opt.startswith('bridgename='):
-					bridge = opt.split('=')[1]
-
-					rows = self.owner.db.execute("""select
-						s.mtu from networks net,
-						nodes n, subnets s where
-						n.name = '%s' and
-						n.id = net.node and 
-						net.device = '%s' and
-						net.subnet = s.id """ %
-						(host, bridge))
-
-					if rows:
-						mtu, = self.owner.db.fetchone()
 
 			if self.owner.interface:
 				if self.owner.interface == device:
