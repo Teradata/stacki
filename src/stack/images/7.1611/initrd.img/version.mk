a48c6555162199a1ae7a33b6a53fac20ada4c4b0
Anoop Rajendra <anoop.rajendra@stacki.com>
2017-06-12 16:39:29 -0700

    Add PyMySQL to initrd, and remove gawk

--- src/stack/images/7.1611/initrd.img/version.mk
+++ src/stack/images/7.1611/initrd.img/version.mk
@@ -1,5 +1,5 @@
 ORDER			= 99
 PKGROOT			= /opt/stack/images
 OVERLAY.UPDATE.PKGS	= stack-command stack-pylib \
-	foundation-python foundation-python-xml stack-tracker \
-	lighttpd foundation-gawk
+	foundation-python foundation-python-xml foundation-py-PyMySQL \
+	stack-tracker lighttpd
