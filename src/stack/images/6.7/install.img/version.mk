2d8d8c1084e79381b93dc034f73f9bc2c70a8395
Mason J. Katz <Mason.Katz@Teradata.com>
2017-07-26 20:46:01 -0400

    Backend Kickstart almost there, but anaconda (RH's code) is failing.

--- src/stack/images/6.7/install.img/version.mk
+++
@@ -1,7 +0,0 @@
-ORDER		= 99
-PKGROOT		= /opt/stack/images
-OVERLAY.PKGS	= foundation-py-wxPython foundation-python-xml \
-	foundation-mariadb MegaCLI hpssacli storcli yum stack-pylib \
-	stack-command foundation-python foundation-gawk foundation-redhat \
-	libtiff createrepo nss-softokn-freebl stack-ekv stack-tracker \
-	stack-piece-pipe tigervnc-server stack-wizard eject
