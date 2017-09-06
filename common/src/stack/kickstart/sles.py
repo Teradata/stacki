ad6a71993e76b6f54d022c817937d1a7bd92bda2
Mason J. Katz <mason.katz@StackIQ.com>
2017-09-06 12:39:26 -0700

    Moved SLES Frontend XML into pallet Moved rest of SLES Frontend python code over

---
+++ common/src/stack/kickstart/sles.py
@@ -0,0 +1,28 @@
+#! /opt/stack/bin/python
+#
+# @SI_Copyright@
+# @SI_Copyright@
+
+import os
+import sys
+import string
+import syslog
+import stack.api
+import profile
+
+class Profile(profile.ProfileBase):
+
+	def main(self, client):
+
+		report = []
+		cmd = '/opt/stack/bin/stack list host xml %s' % client.addr
+		for line in os.popen(cmd).readlines():
+			report.append(line[:-1])
+
+		if report:
+			out = '\n'.join(report)
+			print('Content-type: application/octet-stream')
+			print('Content-length: %d' % len(out))
+			print('')
+			print(out)
+		
