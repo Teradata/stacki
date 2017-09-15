0fff86a4050c0b49ad4d8a0866091070dcae4a95
Anoop Rajendra <anoop.rajendra@teradata.com>
2017-09-15 19:41:04 -0400

    Added more bootstrap to sles

--- sles/bootstrap.mk
+++ sles/bootstrap.mk
@@ -1,6 +1,6 @@
 GROUPS=32bit Basis-Devel SDK-C-C++
 
-PACKAGES=rpm-build libzip2 apache2 squashfs cmake apache2-devel createrepo asciidoc xmlto readline-devel slang-devel popt-devel
+PACKAGES=rpm-build libzip2 apache2 squashfs cmake apache2-devel createrepo readline-devel slang-devel popt-devel systemd-devel
 
 $(GROUPS):
 	zypper in -y -t pattern $@
