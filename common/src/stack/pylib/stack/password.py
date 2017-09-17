e9afa7edcc2363fb19f74565b57cbfa65ea8ed55
Chris Ladd <caladd@particlestorm.net>
2017-09-16 20:47:51 -0700

    Use urandom for password generation
    I noticed that this file was using normal random for password generation, which is considered insecure. The os.urandom function gives cryptographically secure randomness needed for security.
    
    I also removed the MD5 crypt method for salting, because it is considered insecure due to the speed of modern hardware. Instead, I let the crypt function generate the salt using the strongest method available. If you do need to use MD5 crypt the last line can be replaced with:
    
    salt = crypt.mksalt(crypt.METHOD_MD5)
    return crypt.crypt(c_pw, salt)

--- common/src/stack/pylib/stack/password.py
+++ common/src/stack/pylib/stack/password.py
@@ -7,38 +7,20 @@
 
 import base64
 import crypt
-import random
-import time
+import os
 
 
 class Password:
-	def __init__(self):
-		s = int(time.time() * pow(10, 9))
-		random.seed(s)
-
-	def get_rand(self, num_bytes=16):
-		c = ''
-		for i in range(num_bytes):
-			c = c + chr(random.getrandbits(8))
-			random.seed(int(time.time() * pow(10, 9)))
-		return c.encode()
-
-	def get_salt(self):
-		salt = '$1$'
-		s = self.get_rand()
-		salt = salt + base64.urlsafe_b64encode(s)[0:8].decode()
-		return salt
-
 	def get_cleartext_pw(self, c_pw=None):
 		if not c_pw:
-			c = self.get_rand()
-			c_pw = base64.urlsafe_b64encode(c)
-			c_pw = c_pw.rstrip('='.encode())
+			c = os.urandom(16)
+			c_pw = base64.urlsafe_b64encode(c).rstrip(b'=')
+		
 		return c_pw
 
 	def get_crypt_pw(self, c_pw=None):
 		# if c_pw was not specified, generate a random password
 		if not c_pw:
 			c_pw = self.get_cleartext_pw()
-		salt = self.get_salt()
-		return crypt.crypt(c_pw, salt)
+		
+		return crypt.crypt(c_pw)
