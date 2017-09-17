0bad83ee4c92699a38d2b766e166256943ffda75
Chris Ladd <caladd@particlestorm.net>
2017-09-17 02:45:20 -0700

    Move the decode

--- common/src/stack/pylib/stack/password.py
+++ common/src/stack/pylib/stack/password.py
@@ -14,9 +14,9 @@ class Password:
 	def get_cleartext_pw(self, c_pw=None):
 		if not c_pw:
 			c = os.urandom(16)
-			c_pw = base64.urlsafe_b64encode(c).rstrip(b'=')
+			c_pw = base64.urlsafe_b64encode(c).rstrip(b'=').decode()
 		
-		return c_pw.decode()
+		return c_pw
 
 	def get_crypt_pw(self, c_pw=None):
 		# if c_pw was not specified, generate a random password
