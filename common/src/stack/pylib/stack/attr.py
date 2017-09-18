076a76cd768ae2dbc2e760154b3a4f896d6c5cc6
Mason J. Katz <mason.katz@StackIQ.com>
2017-09-18 18:41:23 -0400

    sane copyrights

--- common/src/stack/pylib/stack/attr.py
+++ common/src/stack/pylib/stack/attr.py
@@ -8,7 +8,7 @@
 #
 # @copyright@
 # Copyright (c) 2006 - 2017 StackIQ Inc.
-# All rights reserved. stacki(r) v4.0 stacki.com
+# All rights reserved. stacki(r) v5.0 stacki.com
 # https://github.com/Teradata/stacki/blob/master/LICENSE.txt
 # @copyright@
 #
