c8b1dd64f59cac5663890ed01a9389583fea9748
Anoop Rajendra <anoop.rajendra@teradata.com>
2017-10-09 16:18:00 -0400

    Add UEFI boot method to the install
    Set up uefi boot method. When the node finishes installation,
    run uefi_boot_method to set the first boot device to network.

---
+++ common/src/stack/uefi-boot-method/uefi_boot_method.py
@@ -0,0 +1,40 @@
+#!/opt/stack/bin/python3
+
+# @copyright@
+# @copyright@
+
+import sys
+import os
+import subprocess
+
+# Check if we are running in efi mode
+if not os.path.isdir('/sys/firmware/efi'):
+	sys.exit()
+
+# Parse the UEFI bootorder and put the installation network as
+# the first boot device. We store this in /opt/stack/etc/uefi_netboot
+netbootfile = '/opt/stack/etc/uefi_netboot'
+if not os.path.isfile(netbootfile):
+	sys.exit()
+
+f = open(netbootfile, 'r')
+netboot = f.read().strip()
+
+# get the current EFI boot order
+efi_proc = subprocess.Popen(['/usr/sbin/efibootmgr'],
+	stdout=subprocess.PIPE)
+out, err = efi_proc.communicate()
+
+for line in out.splitlines():
+	line = line.decode()
+	if line.startswith('BootOrder: '):
+		boot_order = line.strip().replace('BootOrder: ', '')
+		boot_order = boot_order.split(',')
+		break
+
+b = boot_order.index(netboot)
+if b >= 0:
+	boot_order.pop(b)
+boot_order.insert(0, netboot)
+subprocess.call(['/usr/sbin/efibootmgr', '--bootorder', ','.join(boot_order)])
+subprocess.call(['/usr/sbin/efibootmgr', '--bootnext', netboot])
