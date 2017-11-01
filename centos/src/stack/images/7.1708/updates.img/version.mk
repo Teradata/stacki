520df9bb6d9282ee15d9a1bd956b08a91da40da9
Mason J. Katz <Mason.Katz@Teradata.com>
2017-11-01 09:23:54 -0700

    Fix spaces in version.mk these exist so we know what the major packages are and what the requirements are. This will help prevent bloat when require packages change.

--- centos/src/stack/images/7.1708/updates.img/version.mk
+++ centos/src/stack/images/7.1708/updates.img/version.mk
@@ -10,18 +10,18 @@ OVERLAY.PKGS	= \
 	yum \
 	foundation-newt \
 	foundation-python-Flask \
-	foundation-python-itsdangerous \
-	foundation-python-Werkzeug \
-	foundation-python-click \
-	foundation-python-MarkupSafe \
-	foundation-python-Jinja2 \
+		foundation-python-itsdangerous \
+		foundation-python-Werkzeug \
+		foundation-python-click \
+		foundation-python-MarkupSafe \
+		foundation-python-Jinja2 \
 	foundation-python-PyMySQL \
 	foundation-python-configparser \
 	foundation-python-python-daemon \
-	foundation-python-lockfile \
+		foundation-python-lockfile \
 	foundation-python-requests \
-	foundation-python-urllib3 \
-	foundation-python-chardet \
-	foundation-python-certifi \
-	foundation-python-six \
-	foundation-python-idna
+		foundation-python-urllib3 \
+		foundation-python-chardet \
+		foundation-python-certifi \
+		foundation-python-idna \
+	foundation-python-six
