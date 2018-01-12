867c8dd1a2a539967174c142f1b145b473e8e1ab
Mike Bobadilla <mike.bobadilla@teradata.com>
2018-01-12 13:15:38 -0800

    remove copy pasta

--- common/src/stack/command/stack/commands/sync/switch/__init__.py
+++ common/src/stack/command/stack/commands/sync/switch/__init__.py
@@ -15,51 +15,23 @@ class command(stack.commands.HostArgumentProcessor,
 
 class Command(command):
 	"""
+	Uploads config file to switch
 	"""
 	def run(self, params, args):
 
-		(pinghosts,confirmhosts) = self.fillParams([
-		('pinghosts', False),
-		('confirmhosts', False),
-		])
-
-		pinghosts = self.str2bool(pinghosts)
-		confirmhosts = self.str2bool(confirmhosts)
-
-		macs = {}
 		hosts = self.getHostnames(args)
-		_switches = self.call('list.switch', hosts)
-		for switch in self.call('list.host.interface', [s['host'] for s in _switches]):
+		frontend, *xargs = self.db.getHostname('localhost')
+		switches = self.call('list.switch', hosts)
+		for switch in self.call('list.host.interface', [s['host'] for s in switches]):
 
 			# Get frontend ip for tftp address
-			(_frontend, *args) = [host for host in self.call('list.host.interface', ['localhost']) 
-					if host['network'] == switch['network']]
-
-			# Send traffic through the switch first before requesting mac table
-			if pinghosts or confirmhosts:
-				_host_interfaces = [host for host in self.call('list.host.interface', hosts)
-						if host['network'] == switch['network']]
-
-			if pinghosts:
-				for host in _host_interfaces:
-					x = subprocess.Popen(['ping', '-c', '1', host['ip']], stdout=subprocess.PIPE)
-
-			if confirmhosts:
-				_hosts = {}
-				for host in self.call('list.host', hosts):
-						_hosts[host['host']] = host
-
-				for host in _host_interfaces:
-					if host['mac']:
-						macs[host['mac']] = {
-							'port': _hosts[host['host']]['rank'],
-							'host': host['host'],
-							'interface': host['interface']
-						}
-
-			frontend_tftp_address = _frontend['ip']
+			(frontend, *args) = [host for host in self.call('list.host.interface', ['localhost']) 
+				if host['network'] == switch['network']]	
+			frontend_tftp_address = frontend['ip']
 			switch_address = switch['ip']
-			with stack.switch.SwitchDellX1052(switch_address, 'admin', 'admin') as switch:
-				switch.set_tftp_ip(frontend_tftp_address)
-				switch.connect()
-				switch.upload()
+			switch_name = switch['host']
+			with stack.switch.SwitchDellX1052(switch_address, 'admin', 'admin') as _switch:
+				_switch.set_tftp_ip(frontend_tftp_address)
+				_switch.set_filenames(switch_name)
+				_switch.connect()
+				_switch.upload()
