67498deb6beb13f0a7ce023b25a86170b8556ef6
Mike Bobadilla <mike.bobadilla@teradata.com>
2018-01-22 13:58:23 -0800

    stacki output for running config

--- common/src/stack/switch/command/list/switch/config/__init__.py
+++ common/src/stack/switch/command/list/switch/config/__init__.py
@@ -8,7 +8,7 @@ import stack.commands
 import stack.util
 import stack.switch
 
-class command(stack.commands.HostArgumentProcessor,
+class command(stack.commands.SwitchArgumentProcessor,
 	stack.commands.list.command):
 	pass
 
@@ -17,9 +17,8 @@ class Command(command):
 	"""
 	def run(self, params, args):
 
-		hosts = self.getHostnames(args)
-		_switches = self.call('list.switch', hosts)
-		for switch in self.call('list.host.interface', [s['host'] for s in _switches]):
+		_switches = self.getSwitchNames(args)
+		for switch in self.call('list.host.interface',  _switches):
 
 			# Get frontend ip for tftp address
 			(_frontend, *args) = [host for host in self.call('list.host.interface', ['localhost']) 
@@ -28,6 +27,9 @@ class Command(command):
 			frontend_tftp_address = _frontend['ip']
 			switch_address = switch['ip']
 			switch_name = switch['host']
+			self.beginOutput()
+
+			# Connect to the switch
 			with stack.switch.SwitchDellX1052(switch_address, switch_name, 'admin', 'admin') as switch:
 				switch.set_tftp_ip(frontend_tftp_address)
 				switch.connect()
@@ -36,10 +38,38 @@ class Command(command):
 				with open('/tftpboot/pxelinux/%s_running_config' % switch_name, 'r') as f:
 					lines = f.readlines()
 					_printline = True
+					_block = {}
 					for line in lines:
 						if 'crypto' in line:
 							break
 
-						if _printline:
-							print(line, end='')
+						if 'gigabitethernet' in line:
+							_block['port'] = line.split('/')[-1].strip()
+
+						if 'switchport' in line and 'access' in line:
+							_, _type, _, _vlan = line.split()
+							_block['type'] = _type
+							_block['vlan'] = _vlan
+
+						if '!' in line and _block:
+							try: 
+								self.addOutput(
+									switch_name,[
+									_block['port'],
+									_block['vlan'],
+									_block['type'],]
+									)
+							except:
+								pass
+							_block = {}
+
+						if False:
+							if _printline:
+								print(line, end='')
+			self.endOutput(header=[
+				'switch',
+				'port',
+				'vlan',
+				'type',
+				])
 
