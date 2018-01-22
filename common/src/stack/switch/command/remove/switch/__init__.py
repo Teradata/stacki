2978657b41ccbdd184c07f20ed5b7ae6b530ade5
Mike Bobadilla <mike.bobadilla@teradata.com>
2018-01-22 10:26:18 -0800

    remove switch command

---
+++ common/src/stack/switch/command/remove/switch/__init__.py
@@ -0,0 +1,31 @@
+# @copyright@
+# Copyright (c) 2006 - 2017 Teradata
+# All rights reserved. Stacki(r) v5.x stacki.com
+# https://github.com/Teradata/stacki/blob/master/LICENSE.txt
+# @copyright@
+
+import stack.commands
+from stack.exception import CommandError, ArgRequired
+
+class command(stack.commands.SwitchArgumentProcessor,
+	      stack.commands.remove.command):
+	pass
+
+class Command(command):
+	"""
+	Remove a new host with appliance type 'switch' from the cluster.
+
+	<arg type='string' name='switch'>
+	A single switch/host name. 
+	</arg>
+
+	"""
+
+	def run(self, params, args):
+
+		if len(args) != 1:
+			raise ArgUnique(self, 'switch')
+		
+		switches = self.getSwitchNames(args)
+
+		self.call('remove.host', switches)
