815845e4deb765a77169dd965c6cd8abbdfac7e7
Mike Bobadilla <mike.bobadilla@teradata.com>
2018-01-22 09:47:49 -0800

    stack add switch command

--- common/src/stack/switch/command/add/switch/__init__.py
+++ common/src/stack/switch/command/add/switch/__init__.py
@@ -5,7 +5,62 @@
 # @copyright@
 
 import stack.commands
-from stack.exception import CommandError
+from stack.exception import CommandError, ParamRequired, ArgUnique
 
 class command(stack.commands.Command):
 	pass
+
+class Command(command):
+	"""
+	Add a new host with appliance type 'switch' to the cluster.
+	This command calls 'add host'. It is a simpler way of setting 
+	the appliance type and the attr 'model'.
+
+	<arg type='string' name='switch'>
+	A single switch/host name. 
+	</arg>
+
+	<param type='string' name='model'>
+	The model number of the switch. Used to call implementations during
+	the switch configuration.
+	</param>
+	"""
+
+	def addSwitch(self, switchname, params):
+		# The model of the switch
+		# Defaults to x1052
+		model, = self.fillParams([
+		("model", "x1052"),
+		])
+
+		# Create a new list of params to add host call
+		# instead of checking for params in two places.
+		passthroughParams = list(map(
+			    lambda x, y: "%s=%s" % (x,y),
+			    params.keys(),
+			    params.values()
+			  ))
+
+		self.call('add.host', [
+			switchname, 
+			"appliance=switch",
+			] + passthroughParams
+			)
+
+
+		# Set the model of the switch to 'model' attr
+		self.call('set.host.attr', [
+			switchname,
+			"attr=model",
+			"value=%s" % model,
+			])
+
+
+	def run(self, params, args):
+
+		if len(args) != 1:
+			raise ArgUnique(self, 'switch')
+		
+		switchname = args[0].lower()
+
+		self.addSwitch(switchname, params)
