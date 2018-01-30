77f3be6f0eabe87cac61b349679e76c3b907d327
Mike Bobadilla <mike.bobadilla@teradata.com>
2018-01-29 16:02:43 -0800

    remove confirmhosts param

--- common/src/stack/switch/command/list/switch/mac/__init__.py
+++ common/src/stack/switch/command/list/switch/mac/__init__.py
@@ -9,22 +9,6 @@ import stack.util
 from stack.switch import SwitchDellX1052
 import subprocess
 
-def confirm_hosts(macs, line):
-	if 'dynamic' in line:
-		(_vlan, _mac, _port, _) = line.split()
-		_switch_port = _port.split('/')[-1]
-		if _mac in macs:
-			_host = macs[_mac]['host']
-			_interface = macs[_mac]['interface']
-			_host_port = macs[_mac]['port']
-			if _host_port == _switch_port:
-				print("%s is connected to port %s with %s" % (_host, _host_port, _interface))
-			else:
-				print("%s is NOT connected to port %s with %s" % (_host, _host_port, _interface))
-		else:
-				print("No machine is connected toswitch port %s" % _switch_port)
-	
-
 class command(stack.commands.SwitchArgumentProcessor,
 	      stack.commands.list.command,
 	      stack.commands.HostArgumentProcessor):
@@ -35,16 +19,15 @@ class Command(command):
 	"""
 	def run(self, params, args):
 
-		(pinghosts,confirmhosts) = self.fillParams([
+		(pinghosts,) = self.fillParams([
 		('pinghosts', False),
-		('confirmhosts', False),
 		])
 
 		pinghosts = self.str2bool(pinghosts)
-		confirmhosts = self.str2bool(confirmhosts)
 
 		macs = {}
 		_switches = self.getSwitchNames(args)
+		self.beginOutput()
 		for switch in self.call('list.host.interface', _switches):
 
 			# Get frontend ip for tftp address
@@ -52,32 +35,16 @@ class Command(command):
 					if host['network'] == switch['network']]
 
 			# Send traffic through the switch first before requesting mac table
-			if pinghosts or confirmhosts:
+			if pinghosts:
 				_host_interfaces = [host for host in self.call('list.host.interface')
 						if host['network'] == switch['network']]
-
-			if pinghosts:
 				for host in _host_interfaces:
 					x = subprocess.Popen(['ping', '-c', '1', host['ip']], stdout=subprocess.PIPE)
-
-			if confirmhosts:
-				_hosts = {}
-				for host in self.call('list.host', hosts):
-						_hosts[host['host']] = host
-
-				for host in _host_interfaces:
-					if host['mac']:
-						macs[host['mac']] = {
-							'port': _hosts[host['host']]['rank'],
-							'host': host['host'],
-							'interface': host['interface']
-						}
 
 			frontend_tftp_address = _frontend['ip']
 			switch_address = switch['ip']
 			switch_name = switch['host']
 
-			self.beginOutput()
 			with SwitchDellX1052(switch_address, switch_name, 'admin', 'admin') as switch:
 				switch.set_tftp_ip(frontend_tftp_address)
 				switch.connect()
@@ -90,7 +57,7 @@ class Command(command):
 					  nodes n, networks nt
 					  where nt.node=n.id
 					  and nt.mac='%s'
-					""" % _mac)
+					""" % _mac)[0]
 					self.addOutput(switch_name, [_port, _mac, _hostname, _interface,  _vlan])
 
-			self.endOutput(header=['switch', 'port',  'mac', 'host', 'interface', 'vlan'])
+		self.endOutput(header=['switch', 'port',  'mac', 'host', 'interface', 'vlan'])
