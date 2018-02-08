6af98d0e163b230b808af305d8731e4f8f27f1d5
Mike Bobadilla <mike.bobadilla@teradata.com>
2018-02-08 15:03:46 -0800

    load switch hosfile

---
+++ common/src/stack/switch/command/load/switch/hostfile/plugin_load_default.py
@@ -0,0 +1,57 @@
+# @copyright@
+# Copyright (c) 2006 - 2018 Teradata
+# All rights reserved. Stacki(r) v5.x stacki.com
+# https://github.com/Teradata/stacki/blob/master/LICENSE.txt
+# @copyright@
+#
+# @rocks@
+
+import sys
+import stack.commands
+from stack.bool import str2bool
+
+
+class Plugin(stack.commands.HostArgumentProcessor, stack.commands.Plugin):
+
+	def provides(self):
+		return 'default'
+
+
+	def removeHosts(self, switch, host):
+		self.owner.call('remove.switch.host',
+			[ switch, 'host=%s' % host ])
+
+
+	def run(self, args):
+		hosts = args
+		existinghosts = self.getHostnames()
+
+		sys.stderr.write('\tAdd Host\n')
+		for host in hosts.keys():
+
+			sys.stderr.write('\t\t%s\r' % host)
+
+			#
+			# add the host if it does exist
+			#
+			if host in existinghosts:
+				switch	= hosts[host].get('switch')
+				name	= hosts[host].get('host')
+				port	= hosts[host].get('port')
+
+				# delete the previous entry in case the interface 
+				# has changed
+				self.owner.call('remove.switch.host', [
+						switch,
+						"host=%s" % name,
+						])
+
+				# add switch host
+				self.owner.call('add.switch.host', [
+						switch,
+						"host=%s" % name,
+						"port=%s" % port,
+						])
+
+			sys.stderr.write('\t\t%s\r' % (' ' * len(host)))
+
