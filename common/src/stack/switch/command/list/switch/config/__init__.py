f471ccce43bfa89dd2c072ea862691c0eb35fd97
Mike Bobadilla <mike.bobadilla@teradata.com>
2018-02-09 12:10:17 -0800

    add implementation to list switch config

--- common/src/stack/switch/command/list/switch/config/__init__.py
+++ common/src/stack/switch/command/list/switch/config/__init__.py
@@ -45,6 +45,7 @@ class Command(command):
 			])
 
 		raw = self.str2bool(raw)
+		self.raw = raw
 
 		_switches = self.getSwitchNames(args)
 
@@ -54,55 +55,9 @@ class Command(command):
 
 		for switch in self.call('list.host.interface',  _switches):
 
-			# Get frontend ip for tftp address
-			try:
-				(_frontend, *args) = [host for host in self.call('list.host.interface', ['localhost'])
-						if host['network'] == switch['network']]
-			except:
-				raise CommandError(self, '"%s" and the frontend do not share a network' % switch['host'])
-
-			frontend_tftp_address = _frontend['ip']
-			switch_address = switch['ip']
 			switch_name = switch['host']
-
-			# Connect to the switch
-			with stack.switch.SwitchDellX1052(switch_address, switch_name, 'admin', 'admin') as switch:
-				switch.set_tftp_ip(frontend_tftp_address)
-				switch.connect()
-				switch.download()
-				
-				with open('/tftpboot/pxelinux/%s_running_config' % switch_name, 'r') as f:
-					lines = f.readlines()
-					_printline = True
-					_block = {}
-					for line in lines:
-						if not raw:
-							if 'crypto' in line:
-								break
-
-							if 'gigabitethernet' in line:
-								_block['port'] = line.split('/')[-1].strip()
-
-							if 'switchport' in line and 'access' in line:
-								_, _type, _, _vlan = line.split()
-								_block['type'] = _type
-								_block['vlan'] = _vlan
-
-							if '!' in line and _block:
-								try:
-									self.addOutput(
-										switch_name,[
-										_block['port'],
-										_block['vlan'],
-										_block['type'],]
-										)
-								except:
-									pass
-								_block = {}
-
-						else:
-							if _printline:
-								print(line, end='')
+			model = self.getHostAttr(switch_name, 'model')
+			self.runImplementation(model, [switch])
 
 		if not raw:
 			self.endOutput(header=[
---
+++ common/src/stack/switch/command/list/switch/config/imp_x1052.py
@@ -0,0 +1,65 @@
+# @copyright@
+# Copyright (c) 2006 - 2018 Teradata
+# All rights reserved. Stacki(r) v5.x stacki.com
+# https://github.com/Teradata/stacki/blob/master/LICENSE.txt
+# @copyright@
+
+import stack.commands
+from stack.exception import CommandError
+from stack.switch import SwitchDellX1052
+
+
+class Implementation(stack.commands.Implementation):
+	def run(self, args):
+
+		switch = args[0]
+
+		# Get frontend ip for tftp address
+		try:
+			(_frontend, *args) = [host for host in self.owner.call('list.host.interface', ['localhost'])
+					if host['network'] == switch['network']]
+		except:
+			raise CommandError(self, '"%s" and the frontend do not share a network' % switch['host'])
+
+		frontend_tftp_address	= _frontend['ip']
+		switch_address		= switch['ip']
+		switch_name		= switch['host']
+
+		# Connect to the switch
+		with stack.switch.SwitchDellX1052(switch_address, switch_name, 'admin', 'admin') as switch:
+			switch.set_tftp_ip(frontend_tftp_address)
+			switch.connect()
+			switch.download()
+			
+			with open('/tftpboot/pxelinux/%s_running_config' % switch_name, 'r') as f:
+				lines = f.readlines()
+				_printline = True
+				_block = {}
+				for line in lines:
+					if not self.owner.raw:
+						if 'crypto' in line:
+							break
+
+						if 'gigabitethernet' in line:
+							_block['port'] = line.split('/')[-1].strip()
+
+						if 'switchport' in line and 'access' in line:
+							_, _type, _, _vlan = line.split()
+							_block['type'] = _type
+							_block['vlan'] = _vlan
+
+						if '!' in line and _block:
+							try:
+								self.owner.addOutput(
+									switch_name,[
+									_block['port'],
+									_block['vlan'],
+									_block['type'],]
+									)
+							except:
+								pass
+							_block = {}
+
+					else:
+						if _printline:
+							print(line, end='')
