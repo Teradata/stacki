d49baf70f966962b573719f8b626d5b87467ed75
Mason J. Katz <Mason.Katz@Teradata.com>
2018-02-13 12:42:09 -0800

    fix some copyrights

--- common/src/stack/command/stack/commands/set/bootaction/__init__.py
+++ common/src/stack/command/stack/commands/set/bootaction/__init__.py
@@ -1,5 +1,8 @@
-# @SI_Copyight@
-# @SI_Copyight@
+# @copyright@
+# Copyright (c) 2006 - 2018 Teradata
+# All rights reserved. Stacki(r) v5.x stacki.com
+# https://github.com/Teradata/stacki/blob/master/LICENSE.txt
+# @copyright@
 
 from stack.exception import ArgRequired, ArgUnique, ParamValue, CommandError
 import stack.commands
