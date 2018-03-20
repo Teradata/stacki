a396a1406a0bb52357c4c632a254d571c13ba80d
Anoop Rajendra <anoop.rajendra@teradata.com>
2018-03-20 11:22:19 -0700

    Change interface "default" behaviour :network: :bus: Setting an interface to default causes 2 events. 1. Set the shortname of the host to the default interface 2. Set the default routing for the host
    Change behaviour so that 1 can be overridden by setting an
    interface option. Setting interface option to "shortname" causes
    the shortname to be assigned to that interface instead.

--- common/src/stack/command/stack/commands/report/host/__init__.py
+++ common/src/stack/command/stack/commands/report/host/__init__.py
@@ -13,6 +13,7 @@
 import stack.commands
 import stack.text
 import os.path
+import shlex
 
 
 class command(stack.commands.HostArgumentProcessor,
@@ -35,53 +36,92 @@ class Command(command):
 		self.addOutput(None, stack.text.DoNotEdit())
 		self.addOutput(None, '#  Site additions go in /etc/hosts.local\n')
 		self.addOutput(None, '127.0.0.1\tlocalhost.localdomain\tlocalhost\n')
-		aliases = {}
 		zones = {}
+		aliases = {}
+
+		# Populate the zone map : network->zone
 		for row in self.call('list.network'):
 			zones[row['network']] = row['zone']
-	
-		for row in self.call('list.host.interface'):
-			ip = row['ip']
-			if not ip:
-				continue
 
-			# TODO (maybe)
-			#
-			# The name of the interface should be the name
-			# in the interface list (not from nodes
-			# table).  If this doesn't exist than use the
-			# name in the nodes table.  But always use the
-			# zone from the networks table.
-			#
-			# Don't do anything right now, this has
-			# implications on the dhcpd.conf, dns, and
-			# spreadsheet loading, and who knows what
-			# else.
-			host    = row['host']
-			network = row['network']
-			default = row['default']
+		# Populate the host -> interface -> aliases map
+		for row in self.call('list.host.alias'):
+			host = row['host']
 			interface = row['interface']
+			if host not in aliases:
+				aliases[host] = {}
+			if interface not in aliases[host]:
+				aliases[host][interface] = []
+			aliases[host][interface].append(row['alias'])
 
-			aliases[host] = []
-			for row in self.call('list.host.alias',
-						[ '%s interface=%s' % (host, interface) ]):
-				if row['host'] == host and row['interface'] == interface:
-					aliases[host].append(row['alias'])
+		hosts = {}
 
-			if network:
-				zone = zones[network]
-			else:
-				zone = None
+		# Get a list of all interface, and populate the host map
+		# host -> interfaces.
+		# {"hostname":[
+		#	{"ip":"1.2.3.4", "interface":"eth0", "zone":"domain.com","default":True/None, "shortname":True/False},
+		#	{"ip":"2.3.4.5", "interface":"eth1", "zone":"domain2.com","default":True/None, "shortname":True/False},
+		#	]}
+		interfaces = self.call('list.host.interface')
+		for row in interfaces:
+			if not row['ip']:
+				continue
+			# Each interface dict contains interface name,
+			# zone,whether the interface is the default one,
+			# and whether the shortname should be assigned
+			# to that interface
+			host = row['host']
+			if host not in hosts:
+				hosts[host] = []
+			h = {}
+			h['ip'] = row['ip']
+			h['interface'] = row['interface']
+			h['zone'] = zones[row['network']]
+			h['default'] = row['default']
+			h['shortname'] = False
+			if 'options' in row and row['options']:
+				options = shlex.split(row['options'])
+				for option in options:
+					if option.strip() == 'shortname':
+						h['shortname']= True
+			hosts[host].append(h)
 
-			names = []
-			if zone:
-				names.append('%s.%s' % (host, zone))
-			if default:
-				names.append(host)
-			if host in aliases:
-				for alias in aliases.get(host):
-					names.append(alias)
-			self.addOutput(None, '%s\t%s' % (ip, ' '.join(names)))
+
+		for host in hosts:
+			# Check if any interface for the host has
+			# shortname set to true
+			shortname_exists = False
+			l = list(filter(lambda x: x['shortname'], hosts[host]))
+			if len(l):
+				shortname_exists = True
+
+			# For each interface in the host, get ip, zone, and names,
+			# default, and shortname info
+			for row in hosts[host]:
+				ip = row['ip']
+				zone = row['zone']
+				default = row['default']
+				interface = row['interface']
+				shortname = row['shortname']
+				names = []
+				# Get the FQDN
+				if zone:
+					names.append('%s.%s' % (host, zone))
+				# If shortname for an interface is set to true,
+				# set this interface to have the shortname
+				if shortname_exists and shortname:
+					names.append(host)
+				# If shortname is not set for any interface
+				# set the default interface to have the shortname
+				if default and not shortname_exists:
+					names.append(host)
+
+				# Add any interface specific aliases
+				if host in aliases:
+					if interface in aliases[host]:
+						for alias in aliases[host].get(interface):
+							names.append(alias)
+				# Write it all
+				self.addOutput(None, '%s\t%s' % (ip, ' '.join(names)))
 
 
 		# Finally, add the hosts.local file to the list
