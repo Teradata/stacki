f2c9734b499777e502a42aeda9e337e06e3d1bb9
Anoop Rajendra <anoop.rajendra@teradata.com>
2018-03-20 11:24:43 -0700

    remove unused package
    check for the frontend correctly
    
    lets check if its the frontend properly :sparkles:

--- common/src/stack/command/stack/commands/add/host/route/__init__.py
+++ common/src/stack/command/stack/commands/add/host/route/__init__.py
@@ -11,7 +11,6 @@
 # @rocks@
 
 import stack.commands
-import socket
 import subprocess
 from stack.exception import CommandError
 
@@ -124,7 +123,7 @@ class Command(stack.commands.add.host.command):
 			#
 			# if host is frontend and sync now, add route to routing table
 			#
-			if host == socket.gethostname():
+			if host in self.getHostnames(['localhost']):
 				if syncnow:
 					add_route = ['route', 'add', '-host', address]
 
