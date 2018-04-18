b3db00dda6f050f5eb62d2799e73e0e89e85a09f
Mason J. Katz <Mason.Katz@Teradata.com>
2018-04-18 08:20:52 -0700

    5.1rc2 start

--- version.mk
+++ version.mk
@@ -1,5 +1,5 @@
 export ROLL		= stacki
-export ROLLVERSION	= 5.1rc1
+export ROLLVERSION	= 5.1rc2
 #export ROLLVERSION	= 5.0_$(shell date +%Y%m%d)_$(shell git rev-parse --short HEAD)
 export RELEASE		= $(shell $(STACKBUILD.ABSOLUTE)/bin/os-release)
 
