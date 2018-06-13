937278ccea002c27d2dfe11488061dc48e83f185
knutsonchris <knutsoonchris@gmail.com>
2018-06-13 16:01:39 -0700

    added plugin_appliance and plugin_host - partial functionality

---
+++ common/src/stack/command/stack/commands/load/json/plugin_appliance.py
@@ -0,0 +1,71 @@
+# @copyright@
+# Copyright (c) 2006 - 2018 Teradata
+# All rights reserved. Stacki(r) v5.x stacki.com
+# https://github.com/Teradata/stacki/blob/master/LICENSE.txt
+# @copyright@
+
+import stack.commands
+import json
+
+class Plugin(stack.commands.Plugin):
+
+	def provides(self):
+		return 'appliance'
+
+	def run(self, args):
+
+	#check if the user would like to load appliance data
+		for item in self.owner.data:
+			if item == 'appliance':
+				import_data = self.owner.data[item]
+			else:
+				print('error no appliance data in json file')
+				return
+	
+		#add each appliance then assign its various values to it
+		for appliance in import_data:
+			appliance_name = appliance['name']
+			try:
+				self.owner.command('add.appliance', [ appliance_name ])
+			except Exception as e:
+				print(f'error adding appliance {appliance_name}: {e}')
+	
+			for attr in appliance['attrs']:
+				try:
+					if attr['type'] == 'shadow':
+						self.owner.command('add.appliance.attr', [ appliance_name, f'attr={attr["attr"]}', f'value={attr["value"]}', 'shadow=True' ])
+					else:
+						self.owner.command('add.appliance.attr', [ appliance_name, f'attr={attr["attr"]}', f'value={attr["value"]}' ])
+				except Exception as e:
+					print(f'error adding appliance attr {attr["attr"]}: {e}')
+	
+			for route in appliance['route']:
+				try:
+					self.owner.command('add.appliance.route', [ appliance_name, f'address={route["network"]}', f'gateway={route["gateway"]}', f'netmask={route["netmask"]}' ])
+				except Exception as e:
+					print(f'error adding appliance route: {e}')
+
+			for rule in appliance['firewall']:
+				try:
+					self.owner.command('add.appliance.firewall', [ appliance_name, f'action={rule["action"]}', f'chain={rule["chain"]}', f'protocol={rule["protocol"]}', f'service={rule["service"]}', f'netowrk={rule["network"]}', f'output-network={rule["output-network"]}', f'rulename={rule["name"]}', f'table={rule["table"]}' ])
+				except Exception as e:
+					print(f'error adding firewall rule {rule["name"]}: {e}')
+
+
+			for partition in appliance['partition']:
+				try:
+					print('adding partition...')
+					self.owner.command('add.storage.partition', [ appliance_name, f'device={partition["device"]}', f'options={partition["options"]}', f'mountpoint={partition["mountpoint"]}', f'partid={partition["partid"]}', f'size={partition["size"]}', f'type={partition["fstype"]}' ])
+				except Exception as e:
+					print(f'error adding partition: {e}')
+
+			for controller in appliance['controller']:
+				try:
+					print('adding controller...')
+					self.owner.command('add.storage.controller', [ appliance_name, f'adapter={controller["adapter"]}', f'arrayid={controller["arrayid"]}', f'enclosure={controller["enclosure"]}', f'raidlevel={controller["raidlevel"]}', f'slot={controller["slot"]}' ])
+				except Exception as e:
+					print(f'error adding controller: {e}')
+
+
+
+
