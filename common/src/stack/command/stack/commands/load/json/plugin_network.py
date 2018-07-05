25c5ddf91d245bfc2c25b026570bbd5a1f10b661
knutsonchris <knutsoonchris@gmail.com>
2018-07-05 15:58:12 -0700

    BUGFIX: Use set commands to make sure network and partition infromation are added properly

--- common/src/stack/command/stack/commands/load/json/plugin_network.py
+++ common/src/stack/command/stack/commands/load/json/plugin_network.py
@@ -32,19 +32,53 @@ class Plugin(stack.commands.Plugin):
 		for network in import_data:
 			name = network['name'].strip()
 			try:
-				command = [ name,
-						f'address={network["address"]}',
-						f'mask={network["netmask"]}',
-						f'dns={network["dns"]}',
-						f'mtu={network["mtu"]}',
-						f'pxe={network["pxe"]}' ]
-				if network['gateway']:
-					command.append(f'gateway={network["gateway"]}')
-				if network['zone']:
-					command.append(f'zone={network["zone"]}')
-				self.owner.command('set.network', command)
-				print(f'success setting network {name}')
+				#the add network command requires at least name address and mask
+				#if the network exists already we want to overwrite its information
+				#so we first add the network then set everything
+				self.owner.command('add.network', [ name,
+									f'address={network["address"]}',
+									f'mask={network["netmask"]}' ])
+				print(f'success adding network {name}')
 				self.owner.successes += 1
+			except Exception as e:
+				if 'exists' in str(e):
+					print(f'warning adding network {name}: {e}')
+					self.owner.warnings += 1
+				else:
+					print(f'error adding network {name}: {e}')
+					self.owner.errors += 1
+
+			try:
+				#now we set all of the attributes
+				#if the network existed already in the database we will overwrite everything about it
+				if network['address']:
+					self.owner.command('set.network.address', [ name, f'address={network["address"]}' ])
+					print(f'success adding {name} address')
+					self.owner.successes += 1
+				if network['dns']:
+					self.owner.command('set.network.dns', [ name, f'dns={network["dns"]}' ])
+					print(f'success adding {name} dns')
+					self.owner.successes += 1
+				if network['gateway']:
+					self.owner.command('set.network.gateway', [ name, f'gateway={network["gateway"]}' ])
+					print(f'success adding {name} gateway')
+					self.owner.successes += 1
+				if network['netmask']:
+					self.owner.command('set.network.mask', [ name, f'mask={network["netmask"]}' ])
+					print(f'success adding {name} mask')
+					self.owner.successes += 1
+				if network['mtu']:
+					self.owner.command('set.network.mtu', [ name, f'mtu={network["mtu"]}' ])
+					print(f'success adding {name} mtu')
+					self.owner.successes += 1
+				if network['pxe']:
+					self.owner.command('set.network.pxe', [ name, f'pxe={network["pxe"]}' ])
+					print(f'success adding {name} pxe')
+					self.owner.successes += 1
+				if network['zone']:
+					self.owner.command('set.network.zone', [ name, f'zone={network["zone"]}' ])
+					print(f'success adding {name} zone')
+					self.owner.successes += 1
 
 			except Exception as e:
 				if 'exists' in str(e):
