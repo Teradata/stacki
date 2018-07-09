577702341e3783b7857e0bb5a1e12ae2d8cb69a7
Bill Sanders <billysanders@gmail.com>
2018-07-09 12:00:13 -0700

    BUGFIX: Fix database backup cronjob script
    INTERNAL: Add tests to ensure it doesn't break again

--- common/src/stack/sql/backup-cluster-db.sh
+++ common/src/stack/sql/backup-cluster-db.sh
@@ -8,58 +8,14 @@
 # All rights reserved. Rocks(r) v5.4 www.rocksclusters.org
 # https://github.com/Teradata/stacki/blob/master/LICENSE-ROCKS.txt
 # @rocks@
-#
-# $Log$
-# Revision 1.13  2010/09/07 23:53:09  bruno
-# star power for gb
-#
-# Revision 1.12  2009/05/14 23:41:12  bruno
-# change the database backup to use the foundation
-#
-# Revision 1.11  2009/05/01 19:07:09  mjk
-# chimi con queso
-#
-# Revision 1.10  2008/10/18 00:56:03  mjk
-# copyright 5.1
-#
-# Revision 1.9  2008/03/06 23:41:45  mjk
-# copyright storm on
-#
-# Revision 1.8  2007/07/15 01:39:17  phil
-# explicitly set HOME so that the connection to mysql can done using
-# root's .my.cnf file, which contains the apache password.
-#
-# Revision 1.7  2007/06/23 04:03:25  mjk
-# mars hill copyright
-#
-# Revision 1.6  2006/09/11 22:47:28  mjk
-# monkey face copyright
-#
-# Revision 1.5  2006/08/10 00:09:45  mjk
-# 4.2 copyright
-#
-# Revision 1.4  2005/10/12 18:08:46  mjk
-# final copyright for 4.1
-#
-# Revision 1.3  2005/09/16 01:02:25  mjk
-# updated copyright
-#
-# Revision 1.2  2005/05/24 21:22:00  mjk
-# update copyright, release is not any closer
-#
-# Revision 1.1  2005/03/01 02:03:17  mjk
-# moved from core to base
-#
-# Revision 1.1  2004/04/13 03:21:05  fds
-# Daily mysql cluster backup to RCS (phil's idea).
-#
-#
 
 export HOME=/root
 cd /var/db
 
-/usr/bin/mysqldump -u apache --opt cluster > mysql-backup-cluster
+/usr/bin/mysqldump --defaults-file=/etc/apache.my.cnf --all-databases  > mysql-backup-cluster
+/usr/bin/mysqldump --defaults-file=/opt/stack/etc/django.my.cnf django > mysql-backup-django
 
 # To check in multiple versions, you need to have a lock on the
 # file. RCS will automatically ignore checkins for unchanged files.
 ci -l -t-'stacki Cluster Database' -m`date +"%Y-%m-%d"` mysql-backup-cluster > /dev/null 2>&1
+ci -l -t-'stacki Django Database' -m`date +"%Y-%m-%d"` mysql-backup-django > /dev/null 2>&1
