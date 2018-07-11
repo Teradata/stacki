8a7845e1d0fd6eccd886cca596bf82e5e3cf9a98
Bill Sanders <billysanders@gmail.com>
2018-07-11 15:11:03 -0700

    FEATURE: Add vendor/make to output of 'stack list switch'
    INTERNAL: Add test and 'add switch' fixture

---
+++ test-framework/test-suites/integration/tests/add/test_add_switch.py
@@ -0,0 +1,12 @@
+import pytest
+
+@pytest.mark.usefixtures("add_switch")
+def test_add_switch(host):
+	dirn = '/export/test-files/list/'
+	output_file = 'switch_with_make_and_model_output.json'
+	expected_output = open(dirn + output_file).read()
+
+	result = host.run('stack list switch output-format=json')
+	assert result.rc == 0
+	assert result.stdout.strip() == expected_output.strip()
+
