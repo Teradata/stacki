c0b58eecae352553d864e55c817cec3088b0f1f7
Greg Bruno <greg.bruno@gmail.com>
2018-07-19 17:41:09 +0000

    BUGFIX: When interfaces are removed for the frontend, remove the networking configuration from the live system (the IP configuration will no longer be visible in `ip addr` and the route table will be updated), and remove the ifcfg file from /etc/sysconfig/network/.

---
+++ common/src/stack/command/stack/commands/remove/host/interface/plugin_cleanup.py
@@ -0,0 +1,71 @@
+# @copyright@
+# Copyright (c) 2006 - 2018 Teradata
+# All rights reserved. Stacki(r) v5.x stacki.com
+# https://github.com/Teradata/stacki/blob/master/LICENSE.txt
+# @copyright@
+#
+# @rocks@
+# Copyright (c) 2000 - 2010 The Regents of the University of California
+# All rights reserved. Rocks(r) v5.4 www.rocksclusters.org
+# https://github.com/Teradata/stacki/blob/master/LICENSE-ROCKS.txt
+# @rocks@
+
+import stack.commands
+
+
+class Plugin(stack.commands.Plugin):
+
+	def provides(self):
+		return 'cleanup'
+
+	def requires(self):
+		return [ 'HEAD' ]
+		
+	def removerow(self, interfaceid):
+		self.owner.db.execute("""delete from networks where id = '%s'""" % interfaceid)
+
+	def ifclean(self, interfaceid, interface):
+		#
+		# - turn off the interface
+		# - remove the ifcfg file (if necessary)
+		# - remove it from the live configuration
+		#
+		import subprocess
+		import os
+
+		if ':' in interface:
+			physif = interface.split(':')[0]
+		else:
+			physif = interface
+
+		subprocess.call([ 'ifdown', physif ], stdout=open('/dev/null'),
+			stderr=open('/dev/null'))
+
+		ifcfg = '/etc/sysconfig/network/ifcfg-%s' % physif
+		if os.path.exists(ifcfg):
+			os.remove(ifcfg)
+			
+		self.removerow(interfaceid)
+
+		self.owner.call('sync.host.network', [ 'localhost', 'restart=n' ])
+
+		if os.path.exists(ifcfg):
+			subprocess.call([ 'ifup', physif ], stdout=open('/dev/null'),
+				stderr=open('/dev/null'))
+
+	def run(self, networks):
+		for interfaceid in networks:
+			self.owner.db.execute("""select n.name, net.device
+				from networks net, nodes n
+				where net.id = '%s' and n.id = net.node""" % interfaceid)
+
+			host, interface = self.owner.db.fetchone()
+
+			if self.owner.db.getHostname(host) == self.owner.db.getHostname('localhost'):
+				#
+				# if this is the frontend, so some local cleanup
+				#
+				self.ifclean(interfaceid, interface)
+			else:
+				self.removerow(interfaceid)
+
