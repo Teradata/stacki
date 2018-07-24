933b5a833b9ba9529b98d32b174c0a0f016cdea3
Ryan Santos <ryan.santos@teradata.com>
2018-07-24 16:22:51 -0700

    Undo adding 'sync host network' to 'load switch hostfile'

--- common/src/stack/switch/command/load/switch/hostfile/__init__.py
+++ common/src/stack/switch/command/load/switch/hostfile/__init__.py
@@ -61,9 +61,6 @@ class Command(stack.commands.load.command,
 		args = self.hosts
 		self.runPlugins(args)
 
-		sys.stderr.write('\tSyncing Host\n')
-		self.call('sync.host.network', [*self.hosts.keys()])
-
 		sys.stderr.write('\tSyncing Switch\n')
 		self.call('sync.switch')
 		
