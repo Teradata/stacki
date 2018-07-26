f2665756d76eb2b45ab08805f3f5e259f7b97d79
Taylor Sanchez <xTaylorSanchez@gmail.com>
2018-07-26 15:16:41 -0700

    Put that back before I forget.

--- test-framework/test-suites/integration/run-tests.sh
+++ test-framework/test-suites/integration/run-tests.sh
@@ -5,38 +5,13 @@ set -e
 
 # Make sure we are in the same directory as this script
 cd "$(dirname "${BASH_SOURCE[0]}")"
-single_test=""
-pytest="pytest"
-while [[ "$#" -gt 0 ]]
-do
-    case "$1" in
-        --no-cov)
-            NO_COV=1
-            shift 1
-            ;;
-        --vvv)
-            pytest="pytest -vvv"
-            shift 1
-            ;;
-        *)
-            # Only run specific tests
-            if [ -d "./tests/$1" ] || [ -f  "./tests/$1" ]; then
-                single_test="$1"
-                echo "Only running tests for $1*"
-                NO_COV=1
-                pytest="pytest -vvv"
-            fi
-            shift 1
-            ;;
-    esac
-done
 
 # Run the tests
-if [[ NO_COV ]]
+if [[ $1 == "--no-cov" ]]
 then
-    vagrant ssh frontend -c "sudo -i $pytest /export/tests/$single_test"
+    vagrant ssh frontend -c "sudo -i pytest /export/tests/"
 else
-    vagrant ssh frontend -c "sudo -i $pytest \
+    vagrant ssh frontend -c "sudo -i pytest \
         --cov=/opt/stack/lib/python3.6/site-packages/stack \
         --cov-report term \
         --cov-report html:/export/reports/integration \
