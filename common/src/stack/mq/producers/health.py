b513a532e7c8d0493eb54711c82a8cdf02db37c5
Mason J. Katz <Mason.Katz@Teradata.com>
2018-07-28 14:45:09 -0700

    add psutil / health up = sshd running

--- common/src/stack/mq/producers/health.py
+++ common/src/stack/mq/producers/health.py
@@ -6,6 +6,7 @@
 
 import stack.mq
 import socket
+import psutil
 
 
 class Producer(stack.mq.producers.ProducerBase):
@@ -18,9 +19,18 @@ class Producer(stack.mq.producers.ProducerBase):
 		return 60
 
 	def produce(self):
-		return stack.mq.Message('up', 
-					channel='health', 
-					ttl=self.schedule()*2)
+
+		ssh = False
+		for p in psutil.process_iter(attrs=['name']):
+			if p.info['name'] == 'sshd':
+				ssh = True
+
+		if ssh:
+			return stack.mq.Message('up', 
+						channel='health', 
+						ttl=self.schedule()*2)
+		else:
+			return None
 
 
 
