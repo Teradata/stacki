c437592ecbe7d9872194b9e78a0c44cd6be46c7a
Bill Sanders <billysanders@gmail.com>
2018-08-01 12:23:44 -0700

    FEATURE: Add a check in 'stack report system' for named
    INTERNAL: consolidate redundant code

--- common/src/stack/command/stack/commands/report/system/tests/test_services.py
+++ common/src/stack/command/stack/commands/report/system/tests/test_services.py
@@ -1,23 +1,5 @@
+import pytest
 import os
-
-
-def test_apache_enabled_and_running(host):
-	if os.path.exists('/etc/SuSE-release'):
-		apache = host.service('apache2')
-	else:
-		apache = host.service('httpd')
-
-	assert apache.is_enabled
-	assert apache.is_running
-
-def test_mariadb_enabled_and_running(host):
-	if os.path.exists('/etc/SuSE-release'):
-		service_name = 'mysql'
-	else:
-		service_name = 'mariadb'
-	mariadb = host.service(service_name)
-	assert mariadb.is_enabled
-	assert mariadb.is_running
 
 def test_tftpd_enabled_and_running(host):
 	xinetd = host.service('xinetd')
@@ -30,22 +12,23 @@ def test_tftpd_enabled_and_running(host):
 		err = print("No tftp server is running.")
 		assert err
 
-def test_dhcp_enabled_and_running(host):
-	dhcp = host.service('dhcpd')
-	assert dhcp.is_enabled
-	assert dhcp.is_running
+SERVICES = [
+	'dhcpd',
+	'named',
+	'smq-processor',
+	'smq-producer',
+	'smq-publisher',
+]
 
-def test_smq_processor_enabled_and_running(host):
-	smq_processor = host.service('smq-processor')
-	assert smq_processor.is_enabled
-	assert smq_processor.is_running
+if os.path.exists('/etc/SuSE-release'):
+	SERVICES.append('apache2')
+	SERVICES.append('mysql')
+else:
+	SERVICES.append('httpd')
+	SERVICES.append('mariadb')
 
-def test_smq_producer_enabled_and_running(host):
-	smq_producer = host.service('smq-producer')
-	assert smq_producer.is_enabled
-	assert smq_producer.is_running
-
-def test_smq_publisher_enabled_and_running(host):
-	smq_publisher = host.service('smq-publisher')
-	assert smq_publisher.is_enabled
-	assert smq_publisher.is_running
+@pytest.mark.parametrize("service,", SERVICES)
+def test_service_enabled_and_running(host, service):
+	daemon = host.service(service)
+	assert daemon.is_enabled
+	assert daemon.is_running
