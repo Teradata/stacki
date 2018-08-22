5e81c5efae61f4ac28f9f5fc071aa776f2fe50eb
Bill Sanders <billysanders@gmail.com>
2018-08-22 11:33:29 -0700

    BUGFIX: [WebService] remove the unneeded admin web interface as well as list_dir functionality

--- common/src/stack/ws/restapi/urls.py
+++ common/src/stack/ws/restapi/urls.py
@@ -10,13 +10,8 @@ from stack.restapi.views import *
 
 ftoken = '[A-Za-z0-9_\-\.\/]*'
 
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', StackWS.as_view()),
-    url(r'^dir/(?P<file>%s)$' % (ftoken), list_dir),
     url(r'^login$',log_in),
     url(r'^logout$',log_out),
     url(r'^user$',check_user),
