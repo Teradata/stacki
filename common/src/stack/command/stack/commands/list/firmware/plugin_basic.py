9b4cb3b75297ca306bcac99943a4fbdf49245e64
Stefan D'Costa <stefan.a.dcosta@gmail.com>
2018-09-25 21:41:41 +0000

    FEATURE: stack list firmware lists all the hosts with their corresponding versions installed and versions available to be installed

---
+++ common/src/stack/command/stack/commands/list/firmware/plugin_basic.py
@@ -0,0 +1,54 @@
+# @copyright@
+# Copyright (c) 2006 - 2018 Teradata
+# All rights reserved. Stacki(r) v5.x stacki.com
+# https://github.com/Teradata/stacki/blob/master/LICENSE.txt
+# @copyright@
+
+import stack.commands
+
+
+class Plugin(stack.commands.Plugin):
+
+	def provides(self):
+		return 'basic'
+
+	def run(self, args):
+		(hosts, expanded, hashit) = args
+
+		keys      = [ ] 
+		host_info = dict.fromkeys(hosts)
+		for host in hosts:
+			host_info[host] = []
+
+		if expanded:
+			# This is used by the MessageQ as a permanent handle on
+			# Redis keys. This allows both the networking and names
+			# of hosts to change and keeps the mq happy -- doesn't
+			# break the status in 'list host'.
+			keys.append('id')
+			for name, id in self.db.select('name, id from nodes'):
+				if name in host_info:
+					host_info[name] = [ id ]
+
+		for row in self.db.select(
+			"""
+			n.name, 
+			a.name,
+			o.name, b.name 
+			from nodes n 
+			left join appliances a   on n.appliance     = a.id
+			left join boxes b        on n.box           = b.id 
+			left join environments e on n.environment   = e.id 
+			left join oses o	 on b.os            = o.id
+			"""):
+
+			if row[0] in host_info:
+				host_info[row[0]].extend(row[1:])
+
+		keys.extend(['appliance',
+			     'os', 'box'])
+
+		return { 'keys' : keys, 'values': host_info }
+
+
+RollName = "stacki"
