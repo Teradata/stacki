5643cfc1f59fd37bbd33c8a93efc7afe0e87e1cf
Chris Ladd <chris.ladd@teradata.com>
2018-09-27 14:00:50 -0700

    BUGFIX: `set bootaction` will now either create a new bootaction or override an existing one, if it already exists.
    INTERNAL: SQL fixes and tests for:
    
    * add bootaction (just tests)
    * set access
    * set bootaction
    * set bootaction args
    * set bootaction kernel
    * set bootaction ramdisk

--- common/src/stack/command/stack/commands/set/bootaction/args/__init__.py
+++ common/src/stack/command/stack/commands/set/bootaction/args/__init__.py
@@ -11,49 +11,51 @@ from stack.exception import CommandError
 class Command(stack.commands.set.bootaction.command):
 	"""
 	Updates the args for a bootaction.
-        <arg type='string' name='bootaction' repeat='0' optional='1'>
-        Name of the bootaction that needs to be updated.
-        </arg>
 
-        <param type='string' name='os'>
-        os type of the bootaction.
-        </param>
-        <param type='string' name='type'>
-        type of the bootaction. Can be install or os.
-        </param>
-        <param type='string' name='args' optional='1'>
-        Updated args value.
-        </param>
+	<arg type='string' name='action' repeat='0' optional='0'>
+	Name of the bootaction that needs to be updated.
+	</arg>
 
-        <example cmd='set bootaction args headless args="ip=bootif:dhcp inst.ks=https://10.25.241.117/install/sbin/profile.cgi" type="install" os="redhat"'>
-        Sets the args for bootaction named headless with type="install" and os="redhat"
-        to be "ip=bootif:dhcp inst.ks=https://10.25.241.117/install/sbin/profile.cgi".
-        </example>
+	<param type='string' name='os'>
+	os type of the bootaction.
+	</param>
+
+	<param type='string' name='type'>
+	type of the bootaction. Can be install or os.
+	</param>
+
+	<param type='string' name='args' optional='0'>
+	Updated args value.
+	</param>
+
+	<example cmd='set bootaction args headless args="ip=bootif:dhcp inst.ks=https://10.25.241.117/install/sbin/profile.cgi" type="install" os="redhat"'>
+	Sets the args for bootaction named headless with type="install" and os="redhat"
+	to be "ip=bootif:dhcp inst.ks=https://10.25.241.117/install/sbin/profile.cgi".
+	</example>
 	"""
-	
+
 	def run(self, params, args):
-	
-		(b_args, ) = self.fillParams([ ('args', '', True) ])
 		(b_action, b_type, b_os) = self.getBootActionTypeOS(params, args)
+
+		(b_args, ) = self.fillParams([ ('args', '', True) ])
 
 		if not self.actionExists(b_action, b_type, b_os):
 			raise CommandError(self, 'action "%s" does not exist' % b_action)
 
 		if b_os:
-			self.db.execute(
-				"""
-				update bootactions 
-				set args = '%s' where
-				os = (select id from oses where name = '%s') and
-				bootname = (select id from bootnames where name = '%s' and type = '%s')
-				""" % (b_args, b_os, b_action, b_type))
+			self.db.execute("""
+				update bootactions set args=%s
+				where os=(
+					select id from oses where name=%s
+				)
+				and bootname=(
+					select id from bootnames where name=%s and type=%s
+				)
+			""", (b_args, b_os, b_action, b_type))
 		else:
-			self.db.execute(
-				"""
-				update bootactions 
-				set args = '%s' where
-				os is NULL and 
-				bootname = (select id from bootnames where name = '%s' and type = '%s')
-				""" % (b_args, b_action, b_type))
-			
-
+			self.db.execute("""
+				update bootactions set args=%s
+				where os is NULL and bootname=(
+					select id from bootnames where name=%s and type=%s
+				)
+			""", (b_args, b_action, b_type))
