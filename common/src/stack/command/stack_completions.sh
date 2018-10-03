30e656d726afe03754f1b2449ab4ffe264a37a52
Shivansh Saini <shivanshs9@gmail.com>
2018-10-03 08:55:37 +0000

    Added TAB completion to stack command.
    Signed-off-by: Shivansh Saini <shivanshs9@gmail.com>

---
+++ common/src/stack/command/stack_completions.sh
@@ -0,0 +1,28 @@
+_stack_completion() {
+	local loc cur opts BASE_DIR
+
+	# BASE_DIR represents the path to the directory containing
+	# all the possible commands in their directories.
+	BASE_DIR="./stack/commands/"
+
+	# Currently typed word
+	cur="${COMP_WORDS[COMP_CWORD]}"
+
+	# Generate directory path from the input command line,
+	# excluding the first word (stack)
+	loc=${BASE_DIR}"$(tr -s ' ' '/' <<< ${COMP_WORDS[@]:1})"
+
+	# Make sure that loc id actually path to a directory
+	while [[ ! -d ${loc} ]]; do
+		loc=$(echo ${loc} | rev | cut -d/ -f2- | rev)
+	done
+
+	# Generate options, depending on loc
+	opts=$(find ${loc} -maxdepth 1 -mindepth 1 -type d -exec basename '{}' \;)
+	opts+=" --debug --version --help"
+
+	# Finally, generate matched results given the options and the current user input.
+	COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
+}
+
+complete -o default -F _stack_completion stack
