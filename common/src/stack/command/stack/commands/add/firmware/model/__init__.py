2169484383d19413eca13552f86bfbb40aed410d
Ryan Causey <ryan.causey@teradata.com>
2018-12-14 17:31:11 -0800

    INTERNAL: Move argument valudation into plugins
    Make sure all duplicate names are output rather than just the first

--- common/src/stack/command/stack/commands/add/firmware/model/__init__.py
+++ common/src/stack/command/stack/commands/add/firmware/model/__init__.py
@@ -10,10 +10,7 @@
 # https://github.com/Teradata/stacki/blob/master/LICENSE-ROCKS.txt
 # @rocks@
 
-
 import stack.commands
-from stack.exception import ArgRequired, CommandError, ParamRequired
-
 
 class Command(stack.commands.add.firmware.command):
 	"""
@@ -33,40 +30,4 @@ class Command(stack.commands.add.firmware.command):
 	"""
 
 	def run(self, params, args):
-		# Require at least one model name
-		if not args:
-			raise ArgRequired(self, 'model')
-
-		make, = self.fillParams(
-			names = [('make', None)],
-			params = params
-		)
-
-		# require a make
-		if make is None:
-			raise ParamRequired(cmd = self, param = 'make')
-
-		# get rid of any duplicate names
-		models = set(args)
-		# ensure the model name doesn't already exist for the given make
-		for model in models:
-			if self.db.count(
-				'''
-				(firmware_model.id), firmware_model.make_id, firmware_make.id
-				FROM firmware_model
-					INNER JOIN firmware_make
-						ON firmware_model.make_id=firmware_make.id
-				WHERE firmware_model.name=%s AND firmware_make.name=%s
-				''',
-				(model, make)
-			) > 0:
-				raise CommandError(cmd = self, msg = f'Firmware model with name {model} already exists for make {make}.')
-
-		# create the make if it doesn't already exist
-		if not self.db.count('(id) FROM firmware_make WHERE name=%s', make):
-			self.call(command = 'add.firmware.make', args = [make])
-
-		# get the ID of the make to associate with
-		make_id = self.db.select('(id) FROM firmware_make WHERE name=%s', make)[0][0]
-		# pass the models and the make_id t the plugins
-		self.runPlugins(args = (models, make_id))
+		self.runPlugins(args = (params, args))
