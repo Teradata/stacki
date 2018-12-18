c07ad617487981f2d1a15a08b4e16cc836e21245
Ryan Causey <ryan.causey@teradata.com>
2018-12-17 17:19:57 -0800

    INTERNAL: first implementation of stack add firmware

--- common/src/stack/command/stack/commands/add/firmware/__init__.py
+++ common/src/stack/command/stack/commands/add/firmware/__init__.py
@@ -21,19 +21,39 @@ class command(stack.commands.add.command):
 
 class Command(command):
 	"""
-	TODO - add actual docstring
 	Adds a firmware image to stacki.
 
-	<arg type='string' name='host'>
-	Host name of machine
+	<arg type='string' name='version'>
+	The firmware version being added. This must be unique within a given firmware family.
 	</arg>
 
-	<param type='string' name='imagepath'>
-	The image that needs to be put in the correct folder structure
+	<!-- TODO: update this to work with more than just local files -->
+	<param type='string' name='source'>
+	The URL pointing to where to retrieve the firmware image. TODO: Currently only supports fetching from local files. Example: file:///export/firmware/my_sweet_image.img
 	</param>
 
-	<example cmd="add host firmware infiniband-10-12 imagepath='/foo/bar/firm.img'">
-	MOVES the firmware image from foo/bar/firm.img to the appropriate directory structure for list and sync firmware commands. Directory Structure: /export/stack/firmware/appliance/make/model
+	<param type='string' name='family'>
+	The firmware family to add this firmware image to. If the family does not already exist, it will be created.
+	</param>
+
+	<param type='string' name='make'>
+	The optional firmware make to add this firmware image to. If the make does not already exist, it will be created. When make is specified, model is also required.
+	</param>
+
+	<param type='string' name='model'>
+	The optional firmware model to add this firmware image to. If the model does not already exist, it will be created. When model is specified, make is also required.
+	</param>
+
+	<param type='string' name='hash'>
+	The optional hash to use when verifying the integrity of the fetched image.
+	</param>
+
+	<param type='string' name='hash_alg'>
+	The optional hash algorithm to use to verify the integrity of the fetched image. If not specified this defaults to MD5.
+	</param>
+
+	<example cmd="add firmware 3.6.5002 source=http://your-sweet-artifactory-builds.labs.teradata.com/firmware/mellanox/img-3.6.5002.img family=mellanox_78xx">
+	Fetches the firmware file from the source, associates it with the mellanox_78xx family of firmwares, sets the version to 3.6.5002, and adds it to be tracked in the stacki database.
 	</example>
 	"""
 
