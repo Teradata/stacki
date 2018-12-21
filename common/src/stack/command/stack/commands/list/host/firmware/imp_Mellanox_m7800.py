5a39eafab10d60e5e180f666cf2cedb4b736c26c
Ryan Causey <ryan.causey@teradata.com>
2018-12-21 14:53:41 -0800

    INTERNAL: first pass on stack list host firmware and it appears to work

--- common/src/stack/command/stack/commands/list/host/firmware/imp_Mellanox_m7800.py
+++ common/src/stack/command/stack/commands/list/host/firmware/imp_Mellanox_m7800.py
@@ -4,25 +4,32 @@
 # https://github.com/Teradata/stacki/blob/master/LICENSE.txt
 # @copyright@
 
+import re
 import stack.commands
 from stack.switch.m7800 import SwitchMellanoxM7800
 
 class Implementation(stack.commands.Implementation):
 
 	def run(self, args):
-		host = args[0]
+		host = args
 		switch_attrs = self.owner.getHostAttrDict(host)
 
 		kwargs = {
-			'username' : switch_attrs[host].get('switch_username'),
-			'password' : switch_attrs[host].get('switch_password')
+			'username' : switch_attrs[host].get('switch_username', None),
+			'password' : switch_attrs[host].get('switch_password', None)
 		}
 
-		kwargs = {k:v for k, v in kwargs.items() if v is not None}
+		kwargs = {key: value for key, value in kwargs.items() if value is not None}
 
 		m7800_switch = SwitchMellanoxM7800(host, **kwargs)
 		m7800_switch.connect()
 		image_listing = m7800_switch.show_images()
-		installed_image = image_listing.installed_images[image_listing.next_boot_partition]
+		installed_image = image_listing.installed_images[image_listing.last_boot_partition]
+		# try to extract the minimal version number from the verbose version string that the switch
+		# returns.
+		match = re.search(r'(?P<version>(\d+\.)+\d+)', installed_image)
+		if match:
+			installed_image = match['version']
+
 		m7800_switch.disconnect()
 		return installed_image
