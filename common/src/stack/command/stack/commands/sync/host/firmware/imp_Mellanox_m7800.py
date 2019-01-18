4868f47d41a933141566d44463ad5f87fa6a6cf5
Ryan Causey <ryan.causey@teradata.com>
2019-01-18 11:03:00 -0800

    INTERNAL: move firmware url function

--- common/src/stack/command/stack/commands/sync/host/firmware/imp_Mellanox_m7800.py
+++ common/src/stack/command/stack/commands/sync/host/firmware/imp_Mellanox_m7800.py
@@ -43,7 +43,7 @@ class Implementation(stack.commands.Implementation):
 		self.owner.notify(notice)
 
 		# calculate the URL the switch can pull this image file from
-		url = self.get_frontend_url(switch_name = switch_name, firmware_file = firmware_file)
+		url = self.owner.get_firmware_url(hostname = switch_name, firmware_file = firmware_file)
 		# connect to the switch and run the firmware upgrade procedure
 		m7800_switch = SwitchMellanoxM7800(switch_name, **kwargs)
 		m7800_switch.connect()
@@ -91,36 +91,3 @@ class Implementation(stack.commands.Implementation):
 				)
 		else:
 			m7800_switch.reload()
-
-	def get_frontend_url(self, switch_name, firmware_file):
-		host_interface_frontend = self.owner.call('list.host.interface', ['a:frontend'])
-
-		switch_networks = set(switch['network'] for switch in self.owner.call('list.host.interface', [switch_name]))
-		frontend_networks = set(frontend['network'] for frontend in host_interface_frontend)
-		common_networks = list(switch_networks & frontend_networks)
-
-		if not common_networks:
-			raise CommandError(
-				cmd = self.owner,
-				msg = (f'{switch_name} does not share a network with the frontend, and thus cannot fetch firmware'
-					   f' from it. Please configure {switch_name} to share a common network with the frontend.')
-			)
-
-		ip_addr = [
-			frontend['ip'] for frontend in host_interface_frontend
-			if frontend['network'] in common_networks and frontend['ip']
-		]
-
-		if not ip_addr:
-			raise CommandError(
-				cmd = self.owner,
-				msg = (f'None of the network interfaces on the frontend attached to the following common networks'
-					   f'have an IP address. Please configure at least one interface to have an IP address on one'
-					   f'of the following networks: {common_networks}')
-			)
-
-		ip_addr = ip_addr[0]
-		# remove the /export/stack prefix from the file path, as /install points to /export/stack
-		firmware_file = Path().joinpath(*(part for part in firmware_file.parts if part not in ('export', 'stack')))
-
-		return f'http://{ip_addr}/install{firmware_file}'
