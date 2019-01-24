9288fc2650d270e3e5a31af8702a04dea34a22a6
Ryan Causey <ryan.causey@teradata.com>
2019-01-23 17:05:15 -0800

    INTERNAL: stack list host firmware running in parallel
    Also change code to handle run_implementations_parallel() returning
    results as namedtuples.

--- common/src/stack/command/stack/commands/list/host/firmware/plugin_version.py
+++ common/src/stack/command/stack/commands/list/host/firmware/plugin_version.py
@@ -7,6 +7,7 @@
 import stack.commands
 from stack.exception import CommandError
 import stack.firmware
+from stack.util import flatten
 
 class Plugin(stack.commands.Plugin):
 
@@ -23,8 +24,8 @@ class Plugin(stack.commands.Plugin):
 		# drop any results that didn't have any errors and aggregate the rest into one exception
 		error_messages = []
 		for error in [
-			value['exception'] for value in results.values()
-			if value is not None and value['exception'] is not None
+			value.exception for value in results.values()
+			if value is not None and value.exception is not None
 		]:
 			# if this looks like a stacki exception type, grab the message from it.
 			if hasattr(error, 'message') and callable(getattr(error, 'message')):
@@ -45,6 +46,7 @@ class Plugin(stack.commands.Plugin):
 		This will raise a CommandError if any of the implementations raised exceptions. The error messages
 		from the implementations will be aggregated into the CommandError raised.
 		"""
+		# remap hosts + their attributes to implementation names
 		mapped_by_imp_name = {
 			f'{host_attrs[host][stack.firmware.MAKE_ATTR]}': {}
 			for host in hosts
@@ -68,6 +70,7 @@ class Plugin(stack.commands.Plugin):
 		This will raise a CommandError if any of the implementations raised exceptions. The error messages
 		from the implementations will be aggregated into the CommandError raised.
 		"""
+		# remap hosts + their attributes to implementation names
 		mapped_by_imp_name = {
 			f'{host_attrs[host][stack.firmware.MAKE_ATTR]}_{host_attrs[host][stack.firmware.MODEL_ATTR]}': {}
 			for host in hosts
@@ -92,33 +95,43 @@ class Plugin(stack.commands.Plugin):
 		# get all host attrs up front
 		host_attrs = self.owner.getHostAttrDict(host = hosts)
 
-		# get hosts with attributes set, and set no results for those missing required attributes
-		hosts_to_check = []
-		for host in hosts:
-			if all(
-				key in host_attrs[host] for key in (stack.firmware.MAKE_ATTR, stack.firmware.MODEL_ATTR)
-			):
-				hosts_to_check.append(host)
-			# if make and model are not set, there's nothing to look up.
-			else:
-				host_results[host].append(None)
+		# get hosts where the make and model are set, otherwise there's nothing to look up.
+		hosts_to_check = [
+			host for host in hosts
+			if all(key in host_attrs[host] for key in (stack.firmware.MAKE_ATTR, stack.firmware.MODEL_ATTR))
+		]
 
 		# run the more specific make + model implementations first as we prefer those results over the more
 		# generic make only implementation. This will raise an exception if any implementations raised an
 		# exception.
 		make_model_results_by_imp = self.run_make_model(hosts = hosts_to_check, host_attrs = host_attrs)
-		remaining_hosts_to_check = [
-			host for host, version in (
-				result.items() for result, exception in (
-					results.items() for implementation, results in make_model_results_by_imp.items()
-					if results is not None
-				) if result is not None
-			) if version is None
-		]
-		# run the more generic make implementation for the remaining hosts. This will raise an exception if
-		# any implementations raised an exception.
-		make_results_by_imp = self.run_model(hosts = remaining_hosts_to_check, host_attrs = host_attrs)
+		make_model_results_by_host = {
+			host: version for host, version in flatten(
+				results.result.items() for results in make_model_results_by_imp.values()
+				if results is not None and results.result is not None
+			) if version is not None
+		}
+		# determine the remaining hosts to run through the more generic implementation based on which hosts
+		# have no version results, and run the more generic make implementation on them. This will raise an
+		# exception if any implementations raised an exception.
+		make_results_by_imp = self.run_make(
+			hosts = [
+				host for host in hosts_to_check if make_model_results_by_host.get(host) is None
+			],
+			host_attrs = host_attrs
+		)
+		# now we get the results by host for hosts which actually had a value returned
+		make_results_by_host = {
+			host: version for host, version in flatten(
+				results.result.items() for results in make_results_by_imp.values()
+				if results is not None and results.result is not None
+			) if version is not None
+		}
+		# unpack into the final set of results by host, preferring the results from the make + model implementation
+		results_by_host = {**make_results_by_host, **make_model_results_by_host}
 
-		host_results[host].append(current_version)
+		# finally fill out all the hosts, setting None for ones that had no results
+		for host in hosts:
+			host_results[host].append(results_by_host.get(host))
 
 		return {'keys': ['current firmware version'], 'values': host_results}
