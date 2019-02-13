e6dae83a68358e11e44499d4e00e9fedbdbbd3fe
Mason J. Katz <Mason.Katz@Teradata.com>
2019-02-13 13:46:50 -0800

    INTERNAL: refresh copyrights for 2019

--- common/src/stack/build/build/src/pallet/bin/manifest-check.py
+++ common/src/stack/build/build/src/pallet/bin/manifest-check.py
@@ -4,7 +4,7 @@
 # when 'make roll' is run
 #
 # @copyright@
-# Copyright (c) 2006 - 2018 Teradata
+# Copyright (c) 2006 - 2019 Teradata
 # All rights reserved. Stacki(r) v5.x stacki.com
 # https://github.com/Teradata/stacki/blob/master/LICENSE.txt
 # @copyright@
