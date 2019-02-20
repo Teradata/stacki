fa7b48b4886d1c9b54617c4707654db95df39eb3
Bill Sanders <billysanders@gmail.com>
2019-02-20 19:47:09 +0000

    FEATURE: Add foundation-golang.
    This pulls in the pre-compiled binaries for go-lang.  Go requires go to build (or go-gcc) neither of which is commonly available from distros yet.

---
+++ common/src/foundation/golang/version.mk
@@ -0,0 +1,5 @@
+ARCHIVENAME	= go
+VERSION		= 1.13.5
+PLATFORM	= linux-amd64
+NAME		= foundation-golang
+RPM.EXTRAS	= "AutoReq: no"
--- common/src/stack/config/etc/profile.d/stack-binaries.sh
+++ common/src/stack/config/etc/profile.d/stack-binaries.sh
@@ -1,11 +1,13 @@
 export STACK_ROOT=/opt/stack
 
 if [ -d $STACK_ROOT/bin ]; then
-	export PATH=$PATH:$STACK_ROOT/bin
+	export PATH=$PATH:$STACK_ROOT/bin:$STACK_ROOT/go/bin
 fi
 
 if [ -d $STACK_ROOT/sbin ]; then
 	export PATH=$PATH:$STACK_ROOT/sbin
 fi
 
-
+if [ -d $STACK_ROOT/lib/pkgconfig ]; then
+	export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$STACK_ROOT/lib/pkgconfig
+fi
