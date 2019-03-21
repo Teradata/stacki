702725c3ec7cd03160eea915e4496232480196c7
Chris Ladd <chris.ladd@teradata.com>
2019-03-21 15:47:00 -0700

    FEATURE: Vagrant based magic to have Stacki build itself

---
+++ tools/iso-builder/README.md
@@ -0,0 +1,40 @@
+# Automated Vagrant based build process
+
+You need to have [Vagrant](https://www.vagrantup.com/) installed, and either
+[VirtualBox](https://www.virtualbox.org/) or Libvirt/KVM. If using Libvirt/KVM, you also need to
+[install the vagrant-libvirt plugin](https://github.com/vagrant-libvirt/vagrant-libvirt#installation)
+and NFS server on the host.
+
+Outside of the Teradata network, only the "redhat7" platform (CentOS based) can be built.
+
+## Setup
+
+Set the following REQUIRED environment variable:
+
+* **PLATFORM** - The platform to build for. One of: "redhat7", "sles12", or "sles11".
+
+Optional environment variables:
+
+* **ISOS** - Folder where the required ISOs for the build process will be downloaded. This can be an absolute path or one relative to the Vagrantfile. Defaults to "." (the `tools/iso-builder/` directory containing the Vagrantfile).
+
+* **IS_RELEASE** - Set to "true" if the date and SHA shouldn't be embedded in the file name.
+
+* **OS_PALLET** - If set to a filename during a "redhat7" platform build, a StackiOS ISO will be generated along with the normal Stacki ISO. The OS pallet needs to already be downloaded into the folder specified in the ISOS environment variable.
+
+* **PYPI_CACHE** - Set to "true" to have the build script use our internal Stacki Builds PyPI cache, which is needed to build the "sles11" platform.
+
+
+## Usage
+
+Set the required environment variables, then change into the `tools/iso-builder` folder and run: `vagrant up`
+
+This will bring up a VM and build the pallet, which will end up in the root of the project.
+
+Run `vagrant destroy -f` to clean up the iso-builder VM.
+
+
+## Example
+
+1. Change into the `tools/iso-builder` directory
+2. Run: `PLATFORM='redhat7' vagrant up`
+3. Once it finishes, you have your ISO. Clean up: `vagrant destroy -f`
---
+++ tools/iso-builder/provision.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# Bail on script errors
+set -e
+
+# Output the commands as they are run
+set -x
+
+# Set up the platform repos and install git
+cd /export/isos/
+INSTALLER_ISOS=http://stacki-builds.labs.teradata.com/installer-isos
+
+if [[ $PLATFORM = "sles11" ]]
+then
+    if [[ ! -f SLES-11-SP3-DVD-x86_64-GM-DVD1.iso ]]
+    then
+        curl -sfSLO --retry 3 $INSTALLER_ISOS/SLES-11-SP3-DVD-x86_64-GM-DVD1.iso
+    fi
+
+    if [[ $(md5sum SLES-11-SP3-DVD-x86_64-GM-DVD1.iso | cut -c 1-32) != "480b70d50cbb538382dc2b9325221e1b" ]]
+    then
+        echo "Error: SLES-11-SP3-DVD-x86_64-GM-DVD1.iso is corrupt"
+        exit 1
+    fi
+
+    if [[ ! -f SLE-11-SP3-SDK-DVD-x86_64-GM-DVD1.iso ]]
+    then
+        curl -sfSLO --retry 3 $INSTALLER_ISOS/SLE-11-SP3-SDK-DVD-x86_64-GM-DVD1.iso
+    fi
+
+    if [[ $(md5sum SLE-11-SP3-SDK-DVD-x86_64-GM-DVD1.iso | cut -c 1-32) != "61d0d77b9d48ad3786fcbba984d77f42" ]]
+    then
+        echo "Error: SLE-11-SP3-SDK-DVD-x86_64-GM-DVD1.iso is corrupt"
+        exit 1
+    fi
+
+    zypper addrepo iso:/?iso=/export/isos/SLES-11-SP3-DVD-x86_64-GM-DVD1.iso os
+    zypper addrepo iso:/?iso=/export/isos/SLE-11-SP3-SDK-DVD-x86_64-GM-DVD1.iso sdk
+    zypper update
+
+    zypper install -y git-core
+
+elif [[ $PLATFORM = "sles12" ]]
+then
+    if [[ ! -f SLE-12-SP3-Server-DVD-x86_64-GM-DVD1.iso ]]
+    then
+        curl -sfSLO --retry 3 $INSTALLER_ISOS/SLE-12-SP3-Server-DVD-x86_64-GM-DVD1.iso
+    fi
+
+    if [[ $(md5sum SLE-12-SP3-Server-DVD-x86_64-GM-DVD1.iso | cut -c 1-32) != "633537da81d270a9548272dfe1fdd20d" ]]
+    then
+        echo "Error: SLE-12-SP3-Server-DVD-x86_64-GM-DVD1.iso is corrupt"
+        exit 1
+    fi
+
+    if [[ ! -f SLE-12-SP3-SDK-DVD-x86_64-GM-DVD1.iso ]]
+    then
+        curl -sfSLO --retry 3 $INSTALLER_ISOS/SLE-12-SP3-SDK-DVD-x86_64-GM-DVD1.iso
+    fi
+
+    if [[ $(md5sum SLE-12-SP3-SDK-DVD-x86_64-GM-DVD1.iso | cut -c 1-32) != "63d5a90ec214f5722acf9c78b3d55043" ]]
+    then
+        echo "Error: SLE-12-SP3-SDK-DVD-x86_64-GM-DVD1.iso is corrupt"
+        exit 1
+    fi
+
+    zypper addrepo iso:/?iso=/export/isos/SLE-12-SP3-Server-DVD-x86_64-GM-DVD1.iso os
+    zypper addrepo iso:/?iso=/export/isos/SLE-12-SP3-SDK-DVD-x86_64-GM-DVD1.iso sdk
+
+    zypper update
+    zypper install -y git-core
+
+else
+    if [[ ! -f CentOS-7-x86_64-Everything-1708.iso ]]
+    then
+        if [[ $INTERNAL == "true" ]]
+        then
+            curl -sfSLO --retry 3 $INSTALLER_ISOS/CentOS-7-x86_64-Everything-1708.iso
+        else
+            curl -sfSLO --retry 3  http://archive.kernel.org/centos-vault/7.5.1804/isos/x86_64/CentOS-7-x86_64-Everything-1708.iso
+        fi
+    fi
+
+    if [[ $(md5sum CentOS-7-x86_64-Everything-1708.iso | cut -c 1-32) != "d23eab94eaa22e3bd34ac13caf923801" ]]
+    then
+        echo "Error: CentOS-7-x86_64-Everything-1708.iso is corrupt"
+        exit 1
+    fi
+
+    mkdir -p /media/cdrom
+    mount -o loop /export/isos/CentOS-7-x86_64-Everything-1708.iso /media/cdrom
+    rm /etc/yum.repos.d/*.repo
+    cat > "/etc/yum.repos.d/dvd.repo" <<EOF
+[dvd]
+name=CentOS-7-x86_64-Everything-1708.iso
+baseurl=file:///media/cdrom/
+gpgcheck=0
+enabled=1
+assumeyes=1
+EOF
+
+    yum update
+    yum -y install git
+fi
