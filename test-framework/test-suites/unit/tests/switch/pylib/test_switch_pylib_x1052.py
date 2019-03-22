2f4ac32b481ccfa97d355bceca8229fab0ca8422
Ryan Causey <ryan.causey@teradata.com>
2019-03-21 18:13:38 -0700

    INTERNAL: Implement library support for listing and updating firmware on dell x1052 switches

--- test-framework/test-suites/unit/tests/switch/pylib/test_switch_pylib_x1052.py
+++ test-framework/test-suites/unit/tests/switch/pylib/test_switch_pylib_x1052.py
@@ -1,8 +1,24 @@
 from unittest.mock import patch, create_autospec, MagicMock, PropertyMock, call
 import pytest
+import itertools
+from collections import namedtuple
+from pathlib import Path
 from stack.expectmore import ExpectMore, ExpectMoreException
 from stack.switch import SwitchException, Switch
 from stack.switch.x1052 import SwitchDellX1052, _requires_regular_console, _requires_configure_console
+
+def powerset(iterable):
+	"""powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)
+
+	Blatantly stolen from itertools recipes.
+	"""
+	combinations_list = list(iterable)
+	return itertools.chain.from_iterable(
+		itertools.combinations(combinations_list, item) for item in range(len(combinations_list)+1)
+	)
+
+
+Input = namedtuple("TestInput", ("input", "result"))
 
 class Testx1052Decorators:
 	"""A test case to hold all tests for the x1052 decorator functions."""
@@ -740,3 +756,311 @@ class TestSwitchDellX1052:
 
 		test_switch = self.SwitchDellX1052UnderTest()
 		assert not test_switch._check_success(results = mock_results, success_string = mock_success_string)
+
+	@pytest.mark.parametrize(
+		"test_inputs",
+		# Generate every combination of software + boot + hardware result to test against.
+		powerset(
+			{
+				"software": Input(
+					input = "SW version    3.0.0.94 ( date  10-Sep-2017 time  22:31:38 )",
+					result = ["3.0.0.94 ( date  10-Sep-2017 time  22:31:38 )"]
+				),
+				"boot": Input(
+					input = "Boot version    1.0.0.25 ( date  05-Apr-2017 time  09:55:19 )",
+					result = ["1.0.0.25 ( date  05-Apr-2017 time  09:55:19 )"]
+				),
+				"hardware": Input(
+					input = "HW version    00.00.04",
+					result = ["00.00.04"]
+				),
+			}.items()
+		)
+	)
+	def test__parse_versions(self, test_inputs):
+		"""Check that parse versions works as expected in all cases."""
+		test_inputs = {key: value for key, value in test_inputs}
+		mock_results = [value.input for value in test_inputs.values()]
+		expected_software = test_inputs.get("software", [])
+		if expected_software:
+			expected_software = expected_software.result
+
+		expected_boot = test_inputs.get("boot", [])
+		if expected_boot:
+			expected_boot = expected_boot.result
+
+		expected_hardware = test_inputs.get("hardware", [])
+		if expected_hardware:
+			expected_hardware = expected_hardware.result
+
+		test_switch = self.SwitchDellX1052UnderTest()
+		results = test_switch._parse_versions(results = mock_results)
+
+		# Expect all of the returned versions to match the expected returned versions.
+		assert expected_software == results.software
+		assert expected_boot == results.boot
+		assert expected_hardware == results.hardware
+
+	def test__check_parsed_versions(self):
+		"""Test that checking parsed versions doesn't raise an exception in the good case."""
+		mock_parsed_versions = namedtuple("MockParsedVersions", ("software", "boot", "hardware"))(
+			software = ["3.0.0.94 ( date  10-Sep-2017 time  22:31:38 )"],
+			boot = ["1.0.0.25 ( date  05-Apr-2017 time  09:55:19 )"],
+			hardware = ["00.00.04"],
+		)
+
+		test_switch = self.SwitchDellX1052UnderTest()
+		test_switch._check_parsed_versions(parsed_versions = mock_parsed_versions)
+
+	@pytest.mark.parametrize(
+		"test_inputs",
+		# Generate every combination of bad result to test against. The result will either be missing, or have duplicate entries.
+		powerset(
+			{
+				"software": ["3.0.0.94 ( date  10-Sep-2017 time  22:31:38 )", "3.0.0.94 ( date  10-Sep-2017 time  22:31:38 )"],
+				"boot": ["1.0.0.25 ( date  05-Apr-2017 time  09:55:19 )", "1.0.0.25 ( date  05-Apr-2017 time  09:55:19 )"],
+				"hardware": ["00.00.04", "00.00.04"],
+			}.items()
+		)
+	)
+	def test__check_parsed_versions_errors(self, test_inputs):
+		"""Test that checking parsed versions raises an exception in all error cases."""
+		test_inputs = {key: value for key, value in test_inputs}
+		mock_parsed_versions = namedtuple("MockParsedVersions", ("software", "boot", "hardware"))(
+			software = test_inputs.get("software", []),
+			boot = test_inputs.get("boot", []),
+			hardware = test_inputs.get("hardware", []),
+		)
+
+		test_switch = self.SwitchDellX1052UnderTest()
+		with pytest.raises(SwitchException):
+			test_switch._check_parsed_versions(parsed_versions = mock_parsed_versions)
+
+	@patch.object(target = SwitchDellX1052, attribute = "_check_parsed_versions", autospec = True)
+	@patch.object(target = SwitchDellX1052, attribute = "_parse_versions", autospec = True)
+	def test_get_versions(self, mock__parse_versions, mock__check_parsed_versions):
+		"""Test that get versions works as expected in the good case."""
+		# Make sure we always return the correct prompt.
+		test_switch = self.SwitchDellX1052UnderTest(
+			match_index_kwargs = {
+				"return_value": SwitchDellX1052.CONSOLE_PROMPTS.index(SwitchDellX1052.CONSOLE_PROMPT)
+			},
+		)
+		mock__parse_versions.return_value = namedtuple("MockParsedVersions", ("software", "boot", "hardware"))(
+			software = ["3.0.0.94 ( date  10-Sep-2017 time  22:31:38 )"],
+			boot = ["1.0.0.25 ( date  05-Apr-2017 time  09:55:19 )"],
+			hardware = ["00.00.04"],
+		)
+		# Set ask to return the mock results
+		test_switch.proc.ask.return_value = [
+			"SW version    3.0.0.94 ( date  10-Sep-2017 time  22:31:38 )",
+			"Boot version    1.0.0.25 ( date  05-Apr-2017 time  09:55:19 )",
+			"HW version    00.00.04",
+		]
+
+		results = test_switch.get_versions()
+
+		# expect the results to match
+		assert mock__parse_versions.return_value.software[0] == results.software
+		assert mock__parse_versions.return_value.boot[0] == results.boot
+		assert mock__parse_versions.return_value.hardware[0] == results.hardware
+		# expect the parsed versions to be validated
+		mock__check_parsed_versions.assert_called_once_with(test_switch, parsed_versions = mock__parse_versions.return_value)
+
+	@patch.object(target = SwitchDellX1052, attribute = "_check_parsed_versions", autospec = True)
+	@patch.object(target = SwitchDellX1052, attribute = "_parse_versions", autospec = True)
+	def test_get_versions_errors(self, mock__parse_versions, mock__check_parsed_versions):
+		"""Test that get versions fails if validating the parsed versions fails."""
+		# Make sure we always return the correct prompt.
+		test_switch = self.SwitchDellX1052UnderTest(
+			match_index_kwargs = {
+				"return_value": SwitchDellX1052.CONSOLE_PROMPTS.index(SwitchDellX1052.CONSOLE_PROMPT)
+			},
+		)
+		mock__parse_versions.return_value = namedtuple("MockParsedVersions", ("software", "boot", "hardware"))(
+			software = ["3.0.0.94 ( date  10-Sep-2017 time  22:31:38 )"],
+			boot = ["1.0.0.25 ( date  05-Apr-2017 time  09:55:19 )"],
+			hardware = ["00.00.04"],
+		)
+		# Set ask to return the mock results
+		test_switch.proc.ask.return_value = [
+			"SW version    3.0.0.94 ( date  10-Sep-2017 time  22:31:38 )",
+			"Boot version    1.0.0.25 ( date  05-Apr-2017 time  09:55:19 )",
+			"HW version    00.00.04",
+		]
+		mock__check_parsed_versions.side_effect = SwitchException("Test error")
+
+		with pytest.raises(SwitchException):
+			test_switch.get_versions()
+
+	@patch(target = "tempfile.NamedTemporaryFile", autospec = True)
+	@patch(target = "stack.switch.x1052.Path", autospec = True)
+	def test__upload_tftp(self, mock_path, mock_named_temporary_file):
+		"""Test that upload tftp works as expected in the good case."""
+		# Make sure we always return the correct prompt.
+		test_switch = self.SwitchDellX1052UnderTest()
+		# Set up the first, second, and third calls to Path() to return a different mock each time.
+		source_file = create_autospec(spec = Path, spec_set = True, instance = True)
+		tftp_dir = create_autospec(spec = Path, spec_set = True, instance = True)
+		temp_file = create_autospec(spec = Path, spec_set = True, instance = True)
+		mock_path.side_effect = (source_file, tftp_dir, temp_file)
+		# Return a mock for the full TFTP directory
+		full_tftp_dir = create_autospec(spec = Path, spec_set = True, instance = True)
+		tftp_dir.__truediv__.return_value = full_tftp_dir
+		mock_timeout = 60
+		mock_source = "foo/bar"
+		mock_destination = "baz"
+
+		test_switch._upload_tftp(source = mock_source, destination = mock_destination, timeout = mock_timeout)
+
+		# Should use the provided path for the source file
+		mock_path.assert_any_call(mock_source)
+		# Should use the resolved full TFTP directory to create the temporary file
+		mock_named_temporary_file.assert_called_once_with(dir = full_tftp_dir.resolve.return_value)
+		# The temporary file should have the resolved source file written to it and then be flushed
+		mock_named_temporary_file.return_value.__enter__.return_value.write.assert_called_once_with(
+			source_file.resolve.return_value.read_bytes.return_value
+		)
+		mock_named_temporary_file.return_value.__enter__.return_value.flush.assert_called_once_with()
+		# The temporary file should have been chmoded using pathlib
+		temp_file.chmod.assert_called_once_with(mode = 0o777)
+		# The file should have been TFTPed to the switch
+		test_switch.proc.ask.assert_called_once_with(
+			cmd = f"copy tftp://{test_switch.stacki_server_ip}/{temp_file.parent.stem}/{temp_file.stem} {mock_destination}",
+			timeout = mock_timeout,
+		)
+
+	@patch.object(target = SwitchDellX1052, attribute = "_check_success", autospec = True)
+	@patch.object(target = SwitchDellX1052, attribute = "_upload_tftp", autospec = True)
+	def test_upload_software(self, mock__upload_tftp, mock__check_success):
+		"""Test that upload software works as expected in the good case."""
+		# Make sure we always return the correct prompt.
+		test_switch = self.SwitchDellX1052UnderTest(
+			match_index_kwargs = {
+				"return_value": SwitchDellX1052.CONSOLE_PROMPTS.index(SwitchDellX1052.CONSOLE_PROMPT)
+			},
+		)
+		mock_software_file = "foo/bar"
+
+		test_switch.upload_software(software_file = mock_software_file)
+
+		# Should use the provided path for the software file, send it to the right destination, and set a long timeout
+		mock__upload_tftp.assert_called_once_with(
+			test_switch,
+			source = mock_software_file,
+			destination = "image",
+			timeout = 600,
+		)
+		# The success should have been checked.
+		mock__check_success.assert_called_once_with(
+			test_switch,
+			results = mock__upload_tftp.return_value,
+			success_string = SwitchDellX1052.COPY_SUCCESS,
+		)
+
+	@patch.object(target = SwitchDellX1052, attribute = "_check_success", autospec = True)
+	@patch.object(target = SwitchDellX1052, attribute = "_upload_tftp", autospec = True)
+	def test_upload_software_error(self, mock__upload_tftp, mock__check_success):
+		"""Test that upload software fails if the success check fails."""
+		# Make sure we always return the correct prompt.
+		test_switch = self.SwitchDellX1052UnderTest(
+			match_index_kwargs = {
+				"return_value": SwitchDellX1052.CONSOLE_PROMPTS.index(SwitchDellX1052.CONSOLE_PROMPT)
+			},
+		)
+		# Make sure _check_success returns False
+		mock__check_success.return_value = False
+
+		with pytest.raises(SwitchException):
+			test_switch.upload_software(software_file = "foo/bar")
+
+	@patch.object(target = SwitchDellX1052, attribute = "_check_success", autospec = True)
+	@patch.object(target = SwitchDellX1052, attribute = "_upload_tftp", autospec = True)
+	def test_upload_boot(self, mock__upload_tftp, mock__check_success):
+		"""Test that upload boot works as expected in the good case."""
+		# Make sure we always return the correct prompt.
+		test_switch = self.SwitchDellX1052UnderTest(
+			match_index_kwargs = {
+				"return_value": SwitchDellX1052.CONSOLE_PROMPTS.index(SwitchDellX1052.CONSOLE_PROMPT)
+			},
+		)
+		mock_software_file = "foo/bar"
+
+		test_switch.upload_boot(boot_file = mock_software_file)
+
+		# Should use the provided path for the boot file, send it to the right destination, and set a long timeout
+		mock__upload_tftp.assert_called_once_with(
+			test_switch,
+			source = mock_software_file,
+			destination = "boot",
+			timeout = 600,
+		)
+		# The success should have been checked.
+		mock__check_success.assert_called_once_with(
+			test_switch,
+			results = mock__upload_tftp.return_value,
+			success_string = SwitchDellX1052.COPY_SUCCESS,
+		)
+
+	@patch.object(target = SwitchDellX1052, attribute = "_check_success", autospec = True)
+	@patch.object(target = SwitchDellX1052, attribute = "_upload_tftp", autospec = True)
+	def test_upload_boot_error(self, mock__upload_tftp, mock__check_success):
+		"""Test that upload boot fails if the success check fails."""
+		# Make sure we always return the correct prompt.
+		test_switch = self.SwitchDellX1052UnderTest(
+			match_index_kwargs = {
+				"return_value": SwitchDellX1052.CONSOLE_PROMPTS.index(SwitchDellX1052.CONSOLE_PROMPT)
+			},
+		)
+		# Make sure _check_success returns False
+		mock__check_success.return_value = False
+
+		with pytest.raises(SwitchException):
+			test_switch.upload_boot(boot_file = "foo/bar")
+
+	def test_reload(self):
+		"""Test that reload works in the good case."""
+		# Make sure we first return the console prompt, then the continue prompt.
+		expected_prompts = [*SwitchDellX1052.CONSOLE_PROMPTS, SwitchDellX1052.CONTINUE_PROMPT, SwitchDellX1052.SHUTDOWN_PROMPT]
+		test_switch = self.SwitchDellX1052UnderTest(
+			match_index_kwargs = {
+				"side_effect": [
+					expected_prompts.index(SwitchDellX1052.CONSOLE_PROMPT),
+					expected_prompts.index(SwitchDellX1052.CONTINUE_PROMPT),
+				]
+			},
+		)
+
+		test_switch.reload()
+
+		# Expect that reboot was asked for and confirmed
+		test_switch.proc.say.assert_any_call(
+			cmd = "reload",
+			prompt = expected_prompts,
+		)
+		test_switch.proc.say.assert_any_call(
+			cmd = "Y",
+			prompt = [*SwitchDellX1052.CONSOLE_PROMPTS, SwitchDellX1052.SHUTDOWN_PROMPT],
+		)
+		# Expect that the process was terminated without any prompts or quit commands
+		test_switch.proc.end.assert_called_once_with(quit_cmd = "", prompt = "")
+
+	def test_reload_no_confirm_prompt(self):
+		"""Test that reload works in the good case."""
+		# Make sure we alway return the console prompt.
+		expected_prompts = [*SwitchDellX1052.CONSOLE_PROMPTS, SwitchDellX1052.CONTINUE_PROMPT, SwitchDellX1052.SHUTDOWN_PROMPT]
+		test_switch = self.SwitchDellX1052UnderTest(
+			match_index_kwargs = {
+				"return_value": expected_prompts.index(SwitchDellX1052.CONSOLE_PROMPT),
+			},
+		)
+
+		test_switch.reload()
+
+		# Expect that reboot was asked for only.
+		test_switch.proc.say.assert_any_call(
+			cmd = "reload",
+			prompt = expected_prompts,
+		)
+		# Expect that the process was terminated without any prompts or quit commands
+		test_switch.proc.end.assert_called_once_with(quit_cmd = "", prompt = "")
