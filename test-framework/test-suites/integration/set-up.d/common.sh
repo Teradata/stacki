0883f78ef54d814ed1461829ef0ba8251c8484a8
Chris Ladd <chris.ladd@teradata.com>
2019-04-03 14:37:12 -0700

    INTERNAL: Pin the python module versions used in test-framework
    The pytest-xdist made a release that doesn't support our version of pytest, breaking the test-framework. Pin the testing related modules so this doesn't happen again.

--- test-framework/test-suites/integration/set-up.d/common.sh
+++ test-framework/test-suites/integration/set-up.d/common.sh
@@ -5,4 +5,4 @@ set -e
 
 # Install pytest-cov on the frontend
 vagrant ssh frontend -c "sudo -i python3 -m ensurepip"
-vagrant ssh frontend -c "sudo -i pip3 install pytest-cov pytest-xdist pytest-rerunfailures pytest-timeout"
+vagrant ssh frontend -c "sudo -i pip3 install pytest-cov==2.6.1 pytest-xdist==1.27.0 pytest-rerunfailures==7.0 pytest-timeout==1.3.3"
