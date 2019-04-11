2e69f024596d87df52423eaa0d54dc33fa86d38c
Ryan Causey <ryan.causey@teradata.com>
2019-04-10 18:47:35 -0700

    INTERNAL: Add commands for managing firmware version_regexes

---
+++ common/src/stack/command/stack/commands/remove/firmware/__init__.py
@@ -0,0 +1,18 @@
+# @copyright@
+# Copyright (c) 2006 - 2018 Teradata
+# All rights reserved. Stacki(r) v5.x stacki.com
+# https://github.com/Teradata/stacki/blob/master/LICENSE.txt
+# @copyright@
+#
+# @rocks@
+# Copyright (c) 2000 - 2010 The Regents of the University of California
+# All rights reserved. Rocks(r) v5.4 www.rocksclusters.org
+# https://github.com/Teradata/stacki/blob/master/LICENSE-ROCKS.txt
+# @rocks@
+
+import stack.commands
+from stack.argument_processors.firmware import FirmwareArgumentProcessor
+
+class command(stack.commands.remove.command, FirmwareArgumentProcessor):
+	pass
+
