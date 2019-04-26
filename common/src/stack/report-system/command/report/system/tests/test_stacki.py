1c9af534db0028cf49e44a7c2d41e57146fd4972
Chris Ladd <chris.ladd@teradata.com>
2019-04-26 12:01:02 -0700

    FEATURE: Improved cluster data output in `report system`
    The `stack report system` command now outputs a section in the end of the output containing all the `list` data collected during the commands. I also added data for `list cart` because it seemed to be missing.
    
    To use this in other tests, simply include the new `report_output` fixture and call it inside of your test function, passing it a "title" for the output and the output itself, both as strings.

--- common/src/stack/report-system/command/report/system/tests/test_stacki.py
+++ common/src/stack/report-system/command/report/system/tests/test_stacki.py
@@ -1,43 +1,8 @@
-import stack.api as api
-
-def test_stack_list_boxes():	
-	output = api.Call('list box')
-	hdr = '\n\nNAME    OS     PALLETS                     CARTS'
-	print(hdr)
-	for o in output:
-		vals = [ str(v) for v in o.values()]
-		print(' '.join(vals))
+import pytest
 
 
-def test_stack_list_pallets():	
-	output = api.Call('list pallet')
-	hdr = '\n\nNAME   VERSION RELEASE ARCH   OS     BOXES'
-	print(hdr)
-	for o in output:
-		vals = [ str(v) for v in o.values()]
-		print(' '.join(vals))
-
-def test_stack_list_networks():	
-	output = api.Call('list network')
-	hdr = '\n\nNETWORK ADDRESS     MASK          GATEWAY      MTU  ZONE  DNS   PXE'
-	print(hdr)
-	for o in output:
-		vals = [ str(v) for v in o.values()]
-		print(' '.join(vals))
-
-
-def test_stack_list_host():	
-	output = api.Call('list host')
-	hdr = '\n\nHOST RACK RANK APPLIANCE OS BOX ENVIRONMENT OSACTION INSTALLACTION STATUS'
-	print(hdr)
-	for o in output:
-		vals = [ str(v) for v in o.values()]
-		print(' '.join(vals))
-
-def test_stack_list_host_interface():
-	output = api.Call('list host interface')
-	hdr = '\n\nHOST INTERFACE DEFAULT NETWORK MAC IP NAME MODULE VLAN OPTIONS CHANNEL'
-	print(hdr)
-	for o in output:
-		vals = [ str(v) for v in o.values()]
-		print(' '.join(vals))
+@pytest.mark.parametrize("command", ["box", "pallet", "cart", "network", "host", "host interface"])
+def test_stack_list(command, host, report_output):
+	result = host.run(f"stack list {command}")
+	assert result.rc == 0
+	report_output(f"list {command}", result.stdout)
