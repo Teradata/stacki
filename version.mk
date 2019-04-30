87122de62b75ba5ada52fe4d79e304bb9b5477c8
Mason J. Katz <Mason.Katz@gmail.com>
2019-04-30 11:13:40 -0700

    INTERNAL: bump version to 05.02.06.07

--- version.mk
+++ version.mk
@@ -1,5 +1,5 @@
 export ROLL		= stacki
-export ROLLVERSION	= 05.02.06.06
+export ROLLVERSION	= 05.02.06.07
 #export ROLLVERSION	= 5.0_$(shell date +%Y%m%d)_$(shell git rev-parse --short HEAD)
 export RELEASE		= $(shell $(STACKBUILD.ABSOLUTE)/bin/os-release)
 
