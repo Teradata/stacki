7a426bdb6509db94ef5391de5d7a36dff34d21b7
bgreenb <bri.greenb@gmail.com>
2019-05-14 16:01:25 -0700

    WIP: Add check when no distribution is found

--- common/src/stack/report-system/command/report/system/tests/test_package_installs.py
+++ common/src/stack/report-system/command/report/system/tests/test_package_installs.py
@@ -6,8 +6,8 @@ from stack.util import _exec
 
 testinfra_hosts = [host['host'] for host in api.Call('list host', ['a:frontend', 'a:backend'])]
 class TestPackageInstall:
-
 	""" Test if the packages stacki installed during setup are still currently installed on the host"""
+
 	def test_package_installs(self, host):
 
 		# Get hostname and see if we can ssh
@@ -19,11 +19,23 @@ class TestPackageInstall:
 			pytest.fail(f'Could not ssh into host')
 
 		config_packages = None
-		installer = None
 		missing_packages = []
 
 		# Get the OS
 		host_os = host.system_info.distribution
+
+		# Check for SLES 11 which uses SuSE-release for the
+		# distribution vs os-release that testinfra reads from
+		if not host_os:
+			try:
+				sles11_release = host.check_output('cat /etc/SuSE-release')
+				for line in sles11_release.splitlines():
+					if '11' in line:
+						host_os = 'sles'
+						break
+
+			except AssertionError:
+				pytest.skip('No OS type found')
 
 		# Different package managers depending on OS
 		if host_os == 'sles':
@@ -32,16 +44,19 @@ class TestPackageInstall:
 		elif host_os == 'centos':
 			installer = 'yum install -y'
 
-		if installer:
+		else:
+			pytest.skip('Unrecongized distribution')
 
-			output = _exec(f'stack list host profile {hostname} chapter=main profile=bash | grep "{installer}"', shell=True).stdout
-			if output:
 
-				# Format the package list to be just the packages without the package manager arguments
-				config_packages = output.replace(installer, '').strip().split(' ')
+		output = _exec(f'stack list host profile {hostname} chapter=main profile=bash | grep "{installer}"', shell=True).stdout
 
-			else:
-				pytest.skip('No stacki installed packages found')
+		if output:
+
+			# Format the package list to be just the packages without the package manager arguments
+			config_packages = output.replace(installer, '').strip().split(' ')
+
+		else:
+			pytest.skip('No stacki installed packages found')
 
 
 		# Use testinfra to check if every package stacki installed is currently
@@ -49,12 +64,13 @@ class TestPackageInstall:
 		for conf_package in config_packages:
 			if not host.package(conf_package).is_installed:
 
-				# If a package isn't installed, use rpm to see if another installed package
-				# provides it instead
+				# Use rpm to see if a package that is supposed to be installed
+				# is provivded by another
 				try:
-					provide_package = host.check_output(f'rpm -q --whatprovides {conf_package}')
+					host.check_output(f'rpm -q --whatprovides {conf_package}')
 
-				# If an rpm doesn't provide the package, an assertion is rasied by testinfra
+				# If an rpm doesn't provide the package, it raises an error code which
+				# triggers an exception by testinfra
 				except AssertionError:
 					missing_packages.append(conf_package)
 
