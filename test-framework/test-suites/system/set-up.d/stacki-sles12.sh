fed57213ce75ffd8bbb0bbdceb10c112bd21704e
Chris Ladd <chris.ladd@teradata.com>
2019-05-21 14:53:10 -0700

    INTERNAL: Handle System test-suite timeouts better
    Set the timeout longer than the Jenkinsfile timeout, so Jenkins has a
    chance to take screenshots. Also, kill the vagrant ups if they are still
    waiting on the backends to come up, so they can be torn down later.

--- test-framework/test-suites/system/set-up.d/stacki-sles12.sh
+++ test-framework/test-suites/system/set-up.d/stacki-sles12.sh
@@ -9,6 +9,6 @@ then
     # Start discovery
     vagrant ssh frontend -c "sudo -i stack enable discovery"
 
-    # Bring up the backends (30 minute timeout)
-    ./set-up.d/_common.sh 30
+    # Bring up the backends (120 minute timeout)
+    ./set-up.d/_common.sh 120
 fi
