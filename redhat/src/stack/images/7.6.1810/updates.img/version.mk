9c2977a1bc1356dfcaa8c2a1e27b7c9cff2c2d92
Greg Bruno <greg.bruno@gmail.com>
2019-06-03 18:45:08 +0000

    FEATURE: Port to Red Hat 7.6.1810

---
+++ redhat/src/stack/images/7.6.1810/updates.img/version.mk
@@ -0,0 +1,26 @@
+PKGROOT		= /opt/stack/images
+OVERLAY.PKGS	= \
+	MegaCLI \
+	foundation-redhat \
+	ludicrous-speed \
+	stack-storage-config \
+	stack-wizard \
+	storcli \
+	yum \
+	foundation-newt \
+	foundation-python-Flask \
+		foundation-python-itsdangerous \
+		foundation-python-Werkzeug \
+		foundation-python-Click \
+		foundation-python-MarkupSafe \
+		foundation-python-Jinja2 \
+	foundation-python-PyMySQL \
+	foundation-python-configparser \
+	foundation-python-python-daemon \
+		foundation-python-lockfile \
+	foundation-python-requests \
+		foundation-python-urllib3 \
+		foundation-python-chardet \
+		foundation-python-certifi \
+		foundation-python-idna \
+	foundation-python-six
