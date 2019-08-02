8163d87a9dba9ca910262bf540ceeb5893d2fed4
Bill Sanders <billysanders@gmail.com>
2019-08-02 12:58:15 -0700

    BUGFIX: when running against a host with an interface with no zone, default to using just the hostname
    Also fix various places where we assume a host, including the FE, will have a 'domainname' attribute.

--- test-framework/test-suites/integration/tests/sync/test_sync_host_network.py
+++ test-framework/test-suites/integration/tests/sync/test_sync_host_network.py
@@ -1,4 +1,5 @@
-def test_sync_host_network_backend(host, add_host, revert_etc):
+def test_sync_host_network_backend_without_ifaces(host, add_host, revert_etc):
+	# test should pass as sync host network ignores backends without networking
 	result = host.run('stack sync host network')
 	assert result.rc == 0
 
