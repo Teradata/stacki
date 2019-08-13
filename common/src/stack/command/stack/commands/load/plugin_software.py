9f767b9caf26d8069ff3ceecaa92ad8fe6e2e2de
Mason J. Katz <Mason.Katz@gmail.com>
2019-08-13 12:18:12 -0700

    FEATURE: 'stack load' includes pallet tags (if pallet exists)
    INTERNAL: stop doing exception handling for key lookups / nuke some dead code

--- common/src/stack/command/stack/commands/load/plugin_software.py
+++ common/src/stack/command/stack/commands/load/plugin_software.py
@@ -15,23 +15,27 @@ class Plugin(stack.commands.Plugin):
 	def requires(self):
 		return ['os']
 
-	def run(self, section):
+	def load_pallet(self, pallets):
+		for p in pallets:
+			pallet = p.get('name')
+			params = {'version': p.get('version'),
+				  'release': p.get('release'),
+				  'arch':    p.get('arch'),
+				  'os':      p.get('os')}
+			for t in p.get('tag', []):
+				params['tag']   = t.get('name')
+				params['value'] = t.get('value')
+				self.owner.stack('add.pallet.tag', pallet, **params)
 
-		try:
-			boxes = section['box']
-		except KeyError:
-			boxes = []
+			
+	def load_box(self, boxes):
 		for b in boxes:
 			box    = b.get('name')
 			params = {'os': b.get('os')}
 
 			self.owner.stack('add.box', box, **params)
 
-			try:
-				pallets = b['pallet']
-			except KeyError:
-				pallets = []
-			for p in pallets:
+			for p in b.get('pallet', []):
 				pallet = p.get('name')
 				params = {'box'    : box,
 					  'version': p.get('version'),
@@ -41,11 +45,7 @@ class Plugin(stack.commands.Plugin):
 
 				self.owner.stack('enable.pallet', pallet, **params)
 
-			try:
-				carts = b['cart']
-			except KeyError:
-				carts = []
-			for c in carts:
+			for c in b.get('cart', []):
 				cart   = c.get('name')
 				params = {'box': box}
 
@@ -53,4 +53,6 @@ class Plugin(stack.commands.Plugin):
 
 
 
-
+	def run(self, section):
+		self.load_box(section.get('box', []))
+		self.load_pallet(section.get('pallet', []))
