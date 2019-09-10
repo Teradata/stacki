07a266be78a7104eef4b9df5f6758ae7c2c988bd
Ryan Causey <ryan.causey@teradata.com>
2019-09-10 14:13:05 -0700

    Add nested resolver to Host for osaction and installaction

--- common/src/stack/graph_ql/app/resolvers/BootactionResolver.py
+++ common/src/stack/graph_ql/app/resolvers/BootactionResolver.py
@@ -10,54 +10,93 @@ import app.db as db
 
 query = QueryType()
 mutation = MutationType()
+host = ObjectType("Host")
+object_types = [host]
 
 @query.field("bootactions")
 def resolve_bootactions(parent, info, os_name=None, boot_name=None, **kwargs):
-	where_conditionals = {
-		f"bootactions.{key}": value
-		for key, value in kwargs.items()
-		if key in ("id", "boot_name_id", "os_id", "kernel", "ramdisk", "args")
-	}
-	ignored_kwargs = set(key.split(".")[-1] for key in where_conditionals.keys()).difference(set(kwargs))
-	if ignored_kwargs:
-		raise ValueError(f"Received unsupported kwargs {ignored_kwargs}")
+    where_conditionals = {
+        f"bootactions.{key}": value
+        for key, value in kwargs.items()
+        if key in ("id", "boot_name_id", "os_id", "kernel", "ramdisk", "args")
+    }
+    ignored_kwargs = set(key.split(".")[-1] for key in where_conditionals.keys()).difference(set(kwargs))
+    if ignored_kwargs:
+        raise ValueError(f"Received unsupported kwargs {ignored_kwargs}")
 
-	cmd = """
-		SELECT
-			bootactions.ID as id,
-			bootactions.BootName as boot_name_id,
-			bootactions.OS as os_id,
-			bootactions.Kernel as kernel,
-			bootactions.Ramdisk ramdisk,
-			bootactions.Args as args
-		FROM bootactions
-		{join}
-		{where}
-	"""
-	join_string = ""
-	if os_name:
-		join_string = "INNER JOIN oses ON bootactions.OS=oses.ID "
-		where_conditionals["oses.Name"] = os_name
+    cmd = """
+        SELECT
+            bootactions.ID as id,
+            bootactions.BootName as boot_name_id,
+            bootactions.OS as os_id,
+            bootactions.Kernel as kernel,
+            bootactions.Ramdisk ramdisk,
+            bootactions.Args as args
+        FROM bootactions
+        {join}
+        {where}
+    """
+    join_string = ""
+    if os_name:
+        join_string = "INNER JOIN oses ON bootactions.OS=oses.ID "
+        where_conditionals["oses.Name"] = os_name
 
-	if boot_name:
-		join_string += "INNER JOIN bootnames on bootactions.BootName=bootnames.ID"
-		where_conditionals[f"bootnames.Name"] = boot_name
+    if boot_name:
+        join_string += "INNER JOIN bootnames on bootactions.BootName=bootnames.ID"
+        where_conditionals[f"bootnames.Name"] = boot_name
 
-	where_string = "WHERE"
-	args = []
-	first = True
-	for key, value in where_conditionals.items():
-		where_string += f" {'' if first else 'and '}{key}=%s"
-		args.append(value)
-		first = False
+    where_string = "WHERE"
+    args = []
+    first = True
+    for key, value in where_conditionals.items():
+        where_string += f" {'' if first else 'and '}{key}=%s"
+        args.append(value)
+        first = False
 
-	if not args:
-		where_string = ""
+    if not args:
+        where_string = ""
 
-	cmd = cmd.format(join=join_string, where=where_string)
-	result, _ = db.run_sql(cmd, args)
+    cmd = cmd.format(join=join_string, where=where_string)
+    result, _ = db.run_sql(cmd, args)
 
-	return result
+    return result
 
+@host.field("osaction")
+def resolve_osaction_from_host(host, info):
+    osaction_id = host.get("osaction_id")
+    if osaction_id is None:
+        return None
 
-object_types = []
+    cmd = """
+        SELECT
+            bootactions.ID as id,
+            bootactions.BootName as boot_name_id,
+            bootactions.OS as os_id,
+            bootactions.Kernel as kernel,
+            bootactions.Ramdisk ramdisk,
+            bootactions.Args as args
+        FROM bootactions
+        WHERE bootactions.ID=%s
+    """
+    result, _ = db.run_sql(cmd, (osaction_id,), fetchone=True)
+    return result
+
+@host.field("installaction")
+def resolve_installaction_from_host(host, info):
+    bootaction_id = host.get("installaction_id")
+    if bootaction_id is None:
+        return None
+
+    cmd = """
+        SELECT
+            bootactions.ID as id,
+            bootactions.BootName as boot_name_id,
+            bootactions.OS as os_id,
+            bootactions.Kernel as kernel,
+            bootactions.Ramdisk ramdisk,
+            bootactions.Args as args
+        FROM bootactions
+        WHERE bootactions.ID=%s
+    """
+    result, _ = db.run_sql(cmd, (bootaction_id,), fetchone=True)
+    return result
