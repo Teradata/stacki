1c504794377b87a1ac74da3a9d19533f201cbf9b
Bill Sanders <billysanders@gmail.com>
2019-10-07 14:51:30 -0700

    BUGFIX: fix bug where we were unable to remove a pallet if the directory contents were already removed.

--- test-framework/test-suites/integration/tests/remove/test_remove_pallet.py
+++ test-framework/test-suites/integration/tests/remove/test_remove_pallet.py
@@ -1,4 +1,5 @@
 import json
+import shutil
 from textwrap import dedent
 
 
@@ -231,3 +232,46 @@ class TestRemovePallet:
 
 		# But the '1.0' version of the pallet should be gone
 		assert not host.file('/export/stack/pallets/minimal/1.0').exists
+
+	def test_remove_partially_removed_pallet(self, host, create_pallet_isos, revert_export_stack_pallets):
+		# Add our minimal pallet
+		result = host.run(f'stack add pallet {create_pallet_isos}/minimal-1.0-sles12.x86_64.disk1.iso')
+		assert result.rc == 0
+
+		expected_pallet_info = [
+			{
+				'name': 'minimal',
+				'version': '1.0',
+				'release': 'sles12',
+				'arch': 'x86_64',
+				'os': 'sles',
+				'boxes': ''
+			}
+		]
+
+		# Make sure it made it to the DB
+		result = host.run('stack list pallet minimal output-format=json')
+		assert result.rc == 0
+		assert json.loads(result.stdout) == expected_pallet_info
+
+		# Sanity check that the pallet files got added to the filesystem
+		assert host.file('/export/stack/pallets/minimal/1.0/sles12/sles/x86_64/roll-minimal.xml').exists
+
+		# now kill part of the tree
+		shutil.rmtree('/export/stack/pallets/minimal/1.0/sles12/sles/x86_64/')
+		assert not host.file('/export/stack/pallets/minimal/1.0/sles12/sles/x86_64/roll-minimal.xml').exists
+
+		# Make sure it is still in the DB
+		result = host.run('stack list pallet minimal output-format=json')
+		assert result.rc == 0
+		assert json.loads(result.stdout) == expected_pallet_info
+
+		# Now remove our test pallet
+		result = host.run('stack remove pallet minimal')
+		assert result.rc == 0
+
+		# now it should be gone from DB and filesys
+		result = host.run('stack list pallet minimal output-format=json')
+		assert result.rc == 255
+		assert result.stderr.startswith('error - "minimal" argument is not a valid pallet ')
+		assert not host.file('/export/stack/pallets/minimal/').exists
