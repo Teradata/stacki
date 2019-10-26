fc0b706a3951f086aab6b022b9bf1716a6c95383
Bill Sanders <billysanders@gmail.com>
2019-10-25 17:03:51 -0700

    FEATURE: make pallets.cgi return os and arch information for pallets.  Refactor pallets.cgi to use probepal

--- common/src/stack/report-system/command/report/system/tests/test_stacki.py
+++ common/src/stack/report-system/command/report/system/tests/test_stacki.py
@@ -1,5 +1,6 @@
 import pytest
 import json
+from operator import itemgetter
 
 @pytest.mark.parametrize("command", ["box", "pallet", "cart", "network", "host", "host interface"])
 def test_stack_list(command, host, report_output):
@@ -26,3 +27,21 @@ def test_stacki_pallets_sane(host):
 
 	# there should be at least one stacki pallet
 	assert stacki_pallets
+
+def test_stacki_central_server(host):
+	result = host.run("stack list pallet output-format=json")
+	assert result.rc == 0
+	palinfo = [
+		dict((k,v)
+		for k, v in p.items() if k != 'boxes')
+		for p in json.loads(result.stdout)
+	]
+
+	result = host.run('curl http://127.0.0.1/install/pallets/pallets.cgi')
+	assert result.rc == 0
+	assert result.stdout
+	central_pallets = json.loads(result.stdout)
+
+	palgetter = itemgetter('name', 'version', 'release', 'arch', 'os')
+
+	assert sorted(palinfo, key=palgetter) == sorted(central_pallets, key=palgetter)
