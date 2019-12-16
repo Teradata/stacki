59520bfd5df9079d87c1ff367c046cfbbe627ef2
Chris Ladd <chris.ladd@teradata.com>
2019-12-16 10:42:01 -0800

    INTERNAL: Pin the version of the coverage module, because the latest version requires sqlite3, which our version of Python is missing

--- test-framework/test-suites/integration/set-up.d/common.sh
+++ test-framework/test-suites/integration/set-up.d/common.sh
@@ -5,4 +5,4 @@ set -e
 
 # Install pytest-cov on the frontend
 vagrant ssh frontend -c "sudo -i python3 -m ensurepip"
-vagrant ssh frontend -c "sudo -i pip3 install pytest-cov==2.8.1 pytest-test-groups==1.0.3 pytest-timeout==1.3.3"
+vagrant ssh frontend -c "sudo -i pip3 install pytest-cov==2.8.1 pytest-test-groups==1.0.3 pytest-timeout==1.3.3 coverage==4.5.4"
