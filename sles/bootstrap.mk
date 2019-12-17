60e1d2105219c89fc0075a3380cca22f16742b0e
Bill Sanders <billysanders@gmail.com>
2019-12-17 22:34:21 +0000

    FEATURE: make bootstrap and make succeed on opensuse 15.1

--- sles/bootstrap.mk
+++ sles/bootstrap.mk
@@ -12,6 +12,13 @@ PKGLIST += rpm-build libzip5 createrepo_c mkisofs libvirt-devel
 PATLIST += devel_basis
 endif
 
+# opensuse 15 will also get the sles15 conditional above,
+# but this (meta) package doesn't exist on SLES
+ifeq ($(DISTRO),openSUSE)
+PKGLIST += patterns-devel-C-C++-devel_C_C++ -site-config
+$(warning now we reached opensuse)
+endif
+
 ifeq ($(RELEASE),sles12)
 PKGLIST += rpm-build libzip2 createrepo cdrkit-cdrtools-compat libvirt-devel
 PATLIST += Basis-Devel
@@ -25,4 +32,6 @@ endif
 bootstrap:
 	../common/src/stack/build/build/bin/package-install -m $(PATLIST)
 	../common/src/stack/build/build/bin/package-install $(PKGLIST)
-
+	@echo $(PKGLIST)
+	zypper addlock site-config
+	if [ "$(DISTRO)" == "openSUSE" ]; then zypper addlock site-config; fi
