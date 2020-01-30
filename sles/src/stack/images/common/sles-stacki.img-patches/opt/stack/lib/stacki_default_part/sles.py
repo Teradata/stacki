9692392294b40297e6fa05b40a2431d39359c9a4
Ryan Causey <ryan.causey@teradata.com>
2020-01-30 21:32:59 +0000

    INTERNAL: Add SLES15 to the default partitioning

--- sles/src/stack/images/common/sles-stacki.img-patches/opt/stack/lib/stacki_default_part/sles.py
+++ sles/src/stack/images/common/sles-stacki.img-patches/opt/stack/lib/stacki_default_part/sles.py
@@ -1,17 +1,24 @@
-sles11_default = [('/', 16000, 'xfs') ,
-	 ('swap', 1000, 'swap') ,
-	 ('/var', 16000, 'xfs') ,
-	 ('/state/partition1', 0, 'xfs')]
+sles11_default = [
+	('/', 16000, 'xfs'),
+	('swap', 1000, 'swap'),
+	('/var', 16000, 'xfs'),
+	('/state/partition1', 0, 'xfs'),
+]
 
-sles11_uefi = [('/boot/efi', 256, 'vfat'),
-	 ('/', 16000, 'xfs') ,
-	 ('swap', 1000, 'swap') ,
-	 ('/var', 16000, 'xfs') ,
-	 ('/state/partition1', 0, 'xfs')]
+sles11_uefi = [
+	('/boot/efi', 256, 'vfat'),
+	('/', 16000, 'xfs'),
+	('swap', 1000, 'swap'),
+	('/var', 16000, 'xfs'),
+	('/state/partition1', 0, 'xfs'),
+]
 
 sles12_default = sles11_default
 
 sles12_uefi = sles11_uefi
 
-default = sles12_default
+sles15_default = sles12_default
 
+sles15_uefi = sles12_uefi
+
+default = sles15_default
