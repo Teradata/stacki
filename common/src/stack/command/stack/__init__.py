5a1d61102b53b8eb13a76d98ebda54aa3626f8dc
bgreenb <bri.greenb@gmail.com>
2020-02-05 14:25:10 -0800

    INTERNAL: Fix the path for pylib when using use-the-src

--- common/src/stack/command/stack/__init__.py
+++ common/src/stack/command/stack/__init__.py
@@ -10,5 +10,16 @@
 # https://github.com/Teradata/stacki/blob/master/LICENSE.txt
 # @copyright@
 
+import os
+import site
+
+site_pkgs_path = [p for p in site.getsitepackages() if p.startswith('/opt/stack/lib/python')][0]
+
+# Needed for use-the-src when symlinking pylib
+# as its location differs in the package vs the src
+# Related to https://bugs.python.org/issue17639
+__path__.append(os.path.join(os.path.split(__file__)[0], '..', '..', 'pylib', 'stack'))
+__path__.append(f'{site_pkgs_path}/stack')
+
 version = 'no-version'
 release = 'no-release'
