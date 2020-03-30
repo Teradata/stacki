0cb2522de1e424d97af9e3c24fba08295ced5264
Mason J. Katz <Mason.Katz@gmail.com>
2020-03-30 16:35:24 -0400

    more stuff to not break centos7 (for now)

--- redhat/bootstrap.mk
+++ redhat/bootstrap.mk
@@ -8,7 +8,7 @@ bootstrap:
 ifeq ($(RELEASE),redhat8)
 	../common/src/stack/build/build/bin/package-install -m "Development Tools" "RPM Development Tools"
 	../common/src/stack/build/build/bin/package-install createrepo genisoimage git \
-		emacs vim httpd-devel libvirt-devel rpm-devel python2 python36
+		emacs vim httpd-devel libvirt-devel rpm-devel python2 python36 yum-downloader
 else
 	../common/src/stack/build/build/bin/package-install -m "Development Tools" "Infrastructure Server"
 	../common/src/stack/build/build/bin/package-install createrepo genisoimage git emacs vim httpd-devel libvirt-devel
