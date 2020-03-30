fd5dda5ba2b3d149a870f3f83942bd0eb87a27fc
Bill Sanders <billysanders@gmail.com>
2020-03-30 16:55:49 -0700

    BUGFIX: make sles pallet patch scripts less likely to fail, which makes run_hooks less likely to fail

--- sles/pallet_hooks/SLES-12sp3-sles12-sles-x86_64/010-add-stacki-squashfs.sh
+++ sles/pallet_hooks/SLES-12sp3-sles12-sles-x86_64/010-add-stacki-squashfs.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-cp -r /opt/stack/pallet-patches/SLES-12sp3-sles12-sles-x86_64/add-stacki-squashfs/* /export/stack/pallets/SLES/12sp3/sles12/sles/x86_64/
+PALLET_DIR=/export/stack/pallets/SLES/12sp3/sles12/sles/x86_64/
+if [[ -d $PALLET_DIR ]]; then
+  cp -r /opt/stack/pallet-patches/SLES-12sp3-sles12-sles-x86_64/add-stacki-squashfs/* $PALLET_DIR
+fi
