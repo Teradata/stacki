3e0b6dfc6dee2decfe3a0f7ed15400ad1049c29d
Mason J. Katz <Mason.Katz@gmail.com>
2020-03-30 15:55:31 -0400

    not dead yet

---
+++ common/src/order-stacki-sles11.mk
@@ -0,0 +1,18 @@
+SRCDIRS=stack/build  \
+	foundation/python  \
+	stack/config  \
+	stack/command  \
+	stack/pylib  \
+	foundation/python-packages  \
+	foundation/MegaCLI  \
+	foundation/StorCLI  \
+	foundation/fio  \
+	foundation/iperf  \
+	foundation/rcs  \
+	foundation/stream  \
+	stack/ludicrous-speed  \
+	stack/mq  \
+	stack/storage-config  \
+	stack/uefi-boot-method  \
+	foundation/zeromq \
+	stack/checklist
