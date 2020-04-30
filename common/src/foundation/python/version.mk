fca71ec45a817e34ababb90ab1be102a119b4751
Bill Sanders <billysanders@gmail.com>
2020-04-30 15:27:37 -0700

    BUGFIX: update foundation python to 3.8.2 (does not update sles11 initrd)

--- common/src/foundation/python/version.mk
+++ common/src/foundation/python/version.mk
@@ -1,4 +1,4 @@
 ORDER      = 0
-VERSION    = 3.8.1
+VERSION    = 3.8.2
 # This was the hacky way I figured out to get %post and %postun in.
 RPM.EXTRAS = "AutoReq: no\\n\\n%post\\n/sbin/ldconfig\\n\\n%postun\\n/sbin/ldconfig"
