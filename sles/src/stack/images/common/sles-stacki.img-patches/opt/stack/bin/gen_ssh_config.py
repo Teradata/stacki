ed6aa3789d43eb9afb07d4368f17440e4ded2c32
Chris Ladd <chris@ladd.dev>
2020-05-01 12:41:14 -0700

    BUGFIX: Get Installer SSH working under SLES 15
    INTERNAL: Generate the installer SSHd host key on the fly, so we no
    longer need to send the FE private.

--- sles/src/stack/images/common/sles-stacki.img-patches/opt/stack/bin/gen_ssh_config.py
+++ sles/src/stack/images/common/sles-stacki.img-patches/opt/stack/bin/gen_ssh_config.py
@@ -15,13 +15,6 @@ f.write("%s\n" % stack_site.authorized_key)
 f.close()
 os.chmod('/authorized_keys',stat.S_IRUSR)
 
-# Write the Host ECDSA Key. Required for
-# the SSH Server to start
-f = open('/etc/ssh/ssh_host_ecdsa_key', 'w')
-f.write("%s\n" % stack_site.host_key)
-f.close()
-os.chmod('/etc/ssh/ssh_host_ecdsa_key',stat.S_IRUSR)
-
 pts = '/dev/pts'
 if not os.path.isdir(pts):
 	os.makedirs(pts)
