e78842cf7ed36902ec03fe22d30865e08f99da6b
Bill Sanders <billysanders@gmail.com>
2020-05-22 16:55:52 -0700

    FEATURE: move pallets and carts to use the same repo jinja templating as repos.

--- common/src/stack/command/stack/commands/report/host/repo/imp_rpm.py
+++ common/src/stack/command/stack/commands/report/host/repo/imp_rpm.py
@@ -7,6 +7,7 @@
 import jinja2
 
 import stack.commands
+from stack.repo import build_repo_files, yum_repo_template
 
 class Implementation(stack.commands.Implementation):
 
@@ -14,37 +15,48 @@ class Implementation(stack.commands.Implementation):
 		host	= args[0]
 		osname	= self.owner.host_attrs[host]['os']
 		box	= self.owner.host_attrs[host]['box']
-		server  = self.owner.host_attrs[host]['Kickstart_PrivateAddress']
-		repo	= []
 
 		if osname == 'sles':
-			filename = '/etc/zypp/repos.d/stacki.repo'
+			repo_location = '/etc/zypp/repos.d/stacki.repo'
 		elif osname == 'redhat':
-			filename = '/etc/yum.repos.d/stacki.repo'
+			repo_location = '/etc/yum.repos.d/stacki.repo'
 
-		repo.append('<stack:file stack:name="%s">' % filename)
+		# if we ever decide to make the repo server ip address more abstract, this is where we'd change it:
+		stacki_http_url_base = 'http://{{ Kickstart_PrivateAddress }}/install'
 
-		for pallet in self.owner.get_box_pallets(box):
-			repo.append('[%s-%s-%s]' % (pallet.name, pallet.version, pallet.rel))
-			repo.append('name=%s %s %s' % (pallet.name, pallet.version, pallet.rel))
-			repo.append('baseurl=http://%s/install/pallets/%s/%s/%s/%s/%s' % \
-				(server, pallet.name, pallet.version, pallet.rel, pallet.os, pallet.arch))
-			repo.append('gpgcheck=0')
-			repo.append('')
+		repo_data = {}
+		# convert the raw pallet and cart data into repo dictionaries
+		for pallet in self.owner.box_data[box]['pallets']:
+			repo_data[pallet.name] = {
+				'alias': f'{pallet.name}-{pallet.version}-{pallet.rel}',
+				'name': f'{pallet.name} {pallet.version} {pallet.rel}',
+				'url': f'{stacki_http_url_base}/pallets/{pallet.name}/{pallet.version}/{pallet.rel}/{pallet.os}/{pallet.arch}',
+				'gpgcheck': False,
+			}
 
-		for o in self.owner.call('list.cart'):
-			if box in o['boxes'].split():
-				repo.append('[%s-cart]' % o['name'])
-				repo.append('name=%s cart' % o['name'])
-				repo.append('baseurl=http://%s/install/carts/%s' % (server, o['name']))
-				repo.append('gpgcheck=0')
-				repo.append('')
+		for cart, _ in self.owner.box_data[box]['carts']:
+			cart_url_base = 'http://{{ Kickstart_PrivateAddress }}/install/carts'
+			repo_data[f'{cart}-cart'] = {
+				'alias': f'{cart}-cart',
+				'name': f'{cart} cart',
+				'url': f'{stacki_http_url_base}/carts/{cart}',
+				'gpgcheck': False,
+			}
 
-		# make a second jinja pass at the repo data, in case it has variables with stacki attributes
-		repo_str = jinja2.Template(self.owner.box_repo_data[box]).render(**self.owner.host_attrs[host])
+		# repo objects are already in the right format so merge that dict here.
+		repo_data = {**repo_data, **self.owner.box_data[box]['repos']}
+
+		# build the template string lines from all the repo objects, add an empty line between each repo
+		repo_lines = '\n\n'.join(build_repo_files({box: repo_data}, yum_repo_template))
+
+		# make a second jinja pass at the repo data, to render any stacki attributes used (eg. Kickstart_PrivateAddress)
+		repo_str = jinja2.Template(repo_lines).render(**self.owner.host_attrs[host])
+
+		repo = []
+		repo.append('<stack:file stack:name="%s">' % repo_location)
 		repo.extend(repo_str.splitlines())
-
 		repo.append('</stack:file>')
+
 		if osname == 'sles':
 			repo.append('zypper clean --all')
 		if osname == 'redhat':
