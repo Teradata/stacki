712568f2d18722a324ccd52cbf0a6f5e6c38277c
Mason J. Katz <Mason.Katz@gmail.com>
2020-07-11 13:39:17 -0700

    pxelinux prepends 01 (arp type) to mac address

--- common/src/stack/command/stack/commands/report/host/bootfile/imp_base.py
+++ common/src/stack/command/stack/commands/report/host/bootfile/imp_base.py
@@ -8,7 +8,7 @@ class PXEImplementation(stack.commands.Implementation):
 
 	def get_tftpboot_filename(self, mac_address):
 		# way more human friendly then the old hex ip address stuff
-		return '-'.join(map(lambda x: x.lower(), mac_address.split(':')))
+		return '01-%s' % '-'.join(map(lambda x: x.lower(), mac_address.split(':')))
 	    
 	def get_sux_header(self, filename):
 		return f'<stack:file stack:name="{filename}" stack:owner="root:apache" stack:perms="0664" stack:rcs="off"><![CDATA['
